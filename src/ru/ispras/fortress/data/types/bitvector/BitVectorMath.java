07fff1b8a66f14371a6043a949fa870b3a8a1c96
Andrei Tatarnikov <andrewt@ispras.ru>
2014-02-13 13:21:18 +0000

    Basic implementation of BitVector operations.

---
+++ src/ru/ispras/fortress/data/types/bitvector/BitVectorMath.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2014 ISPRAS
+ * 
+ * Institute for System Programming of Russian Academy of Sciences
+ * 
+ * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * 
+ * All rights reserved.
+ * 
+ * BitVectorMath.java, Feb 13, 2014 4:34:41 PM Andrei Tatarnikov
+ */
+
+package ru.ispras.fortress.data.types.bitvector;
+
+public final class BitVectorMath
+{
+    private BitVectorMath() {}
+
+    private enum UnOps implements BitVectorAlgorithm.IUnaryOperation
+    {
+        NOT { @Override public byte run(byte v) { return (byte) ~v; } };
+    }
+
+    private enum BinOps implements BitVectorAlgorithm.IBinaryOperation
+    {
+        AND { @Override public byte run(byte lhs, byte rhs) { return (byte) (lhs & rhs); } },
+        OR  { @Override public byte run(byte lhs, byte rhs) { return (byte) (lhs | rhs); } },
+        XOR { @Override public byte run(byte lhs, byte rhs) { return (byte) (lhs ^ rhs); } };
+    }
+
+    public static BitVector and(BitVector lhs, BitVector rhs)
+    {
+        return transform(lhs, rhs, BinOps.AND);
+    }
+
+    public static BitVector or(BitVector lhs, BitVector rhs)
+    {
+        return transform(lhs, rhs, BinOps.OR);
+    }
+
+    public static BitVector xor(BitVector lhs, BitVector rhs)
+    {
+        return transform(lhs, rhs, BinOps.XOR);
+    }
+
+    public static BitVector not(BitVector v)
+    {
+        return transform(v, UnOps.NOT);
+    }
+
+    private static BitVector transform(BitVector lhs, BitVector rhs, BitVectorAlgorithm.IBinaryOperation op)
+    {
+        checkNotNull(lhs);
+        checkNotNull(rhs);
+        checkEqualSize(lhs, rhs);
+
+        final BitVector result = BitVector.newEmpty(lhs.getBitSize());
+        BitVectorAlgorithm.transform(lhs, rhs, result, op);
+
+        return result;
+    }
+
+    private static BitVector transform(BitVector v, BitVectorAlgorithm.IUnaryOperation op)
+    {
+        checkNotNull(v);
+
+        final BitVector result = BitVector.newEmpty(v.getBitSize());
+        BitVectorAlgorithm.transform(v, result, op);
+
+        return result;
+    }
+
+    private static void checkNotNull(Object o)
+    {
+        if (null == o)
+            throw new NullPointerException();
+    }
+
+    private static void checkEqualSize(BitVector lhs, BitVector rhs)
+    {
+        if (lhs.getBitSize() != rhs.getBitSize())
+            throw new IllegalArgumentException(
+                String.format("Bit vector sizes do not match: %d != %d.", lhs.getBitSize(), rhs.getBitSize()));
+    }
+}
