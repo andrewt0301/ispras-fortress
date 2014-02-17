71c7ac450d434e8c219b7f95b8cb02a9faba0312
Andrei Tatarnikov <andrewt@ispras.ru>
2014-02-17 11:10:31 +0000

    AbsCustomOperation was merged with AbsCustomOperationTestCase.

--- src/test/java/ru/ispras/fortress/solver/AbsCustomOperationTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/AbsCustomOperationTestCase.java
@@ -1,7 +1,21 @@
 package ru.ispras.fortress.solver;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.ispras.fortress.data.DataType;
+import ru.ispras.fortress.data.Variable;
+import ru.ispras.fortress.expression.NodeExpr;
+import ru.ispras.fortress.expression.NodeValue;
+import ru.ispras.fortress.expression.NodeVariable;
+import ru.ispras.fortress.expression.StandardOperation;
 import ru.ispras.fortress.solver.common.GenericSolverSampleTestBase;
-import ru.ispras.fortress.solver.samples.AbsCustomOperation;
+import ru.ispras.fortress.solver.constraint.Constraint;
+import ru.ispras.fortress.solver.constraint.ConstraintBuilder;
+import ru.ispras.fortress.solver.constraint.ConstraintKind;
+import ru.ispras.fortress.solver.constraint.Formulas;
+import ru.ispras.fortress.solver.function.FunctionFactory;
+import ru.ispras.fortress.solver.function.FunctionOperation;
 import ru.ispras.fortress.solver.samples.ISampleConstraint;
 
 public class AbsCustomOperationTestCase extends GenericSolverSampleTestBase
@@ -10,5 +24,103 @@ public class AbsCustomOperationTestCase extends GenericSolverSampleTestBase
     public ISampleConstraint createSample()
     {
         return new AbsCustomOperation();
+    }
+
+    /** The constraint as described in the SMT language:
+	<pre>
+    (declare-const a Real)
+    (declare-const b Real)
+    (define-fun ABS ((x Real)) Real( ite( >= x  0.0) x( ~ x)))
+    (assert( =( ABS( ~  5.0))  5.0))
+    (assert( =( ABS  5.0)  5.0))
+    (assert( =( ABS( ~ a))  5.0))
+    (assert( =( ABS b)  5.0))
+    (check-sat)
+    (get-value ( a b))
+    (exit)</pre>
+
+    Expected output: sat ((a (- 5.0))(b (- 5.0)))
+    */
+
+    public static class AbsCustomOperation implements ISampleConstraint
+    {
+        private static final DataType realType = DataType.REAL;
+
+        @Override
+        public Constraint getConstraint()
+        {
+            final Solver solver = SolverId.Z3_TEXT.getSolver();
+            assert solver != null;
+
+            solver.addCustomOperation(
+                FunctionOperation.ABS,
+                FunctionFactory.makeAbs(new Variable("x", realType)));
+
+            final ConstraintBuilder builder = new ConstraintBuilder();
+
+            builder.setName("AbsCustomOperation");
+            builder.setKind(ConstraintKind.FORMULA_BASED);
+            builder.setDescription("AbsCustomOperation constraint");
+
+            final NodeVariable a = new NodeVariable(builder.addVariable("a", realType));
+            final NodeVariable b = new NodeVariable(builder.addVariable("b", realType));
+
+            final Formulas formulas = new Formulas();
+            builder.setInnerRep(formulas);
+
+            formulas.add(
+                new NodeExpr(
+                    StandardOperation.EQ,
+                    new NodeExpr(
+                        FunctionOperation.ABS,
+                        new NodeExpr(StandardOperation.MINUS, new NodeValue(realType.valueOf("5.0",10)))
+                    ),
+                    new NodeValue(realType.valueOf("5.0", 10))
+                )
+            );
+
+            formulas.add(
+                new NodeExpr(
+                    StandardOperation.EQ,
+                    new NodeExpr(
+                        FunctionOperation.ABS,
+                        new NodeValue(realType.valueOf("5.0",10))
+                    ),
+                    new NodeValue(realType.valueOf("5.0", 10))
+                )
+            );
+
+            formulas.add(
+                new NodeExpr(
+                    StandardOperation.EQ,
+                    new NodeExpr(
+                        FunctionOperation.ABS,
+                        new NodeExpr(StandardOperation.MINUS, a)
+                    ),
+                    new NodeValue(realType.valueOf("5.0",10))
+                )
+            );
+
+            formulas.add(
+                new NodeExpr(
+                    StandardOperation.EQ,
+                    new NodeExpr(FunctionOperation.ABS, b),
+                    new NodeValue(realType.valueOf("5.0",10))
+                )
+            );
+
+            return builder.build();
+        }
+
+        @Override
+        public Iterable<Variable> getExpectedVariables()
+        {
+            final List<Variable> result = new ArrayList<Variable>();
+
+            result.add(new Variable("a", realType.valueOf("-5.0", 10)));
+            result.add(new Variable("b", realType.valueOf("-5.0", 10)));
+
+            return result;
+        }
     }
 }
