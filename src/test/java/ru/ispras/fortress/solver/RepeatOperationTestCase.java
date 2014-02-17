38b04ee045bfe4c25d70d89a06bb4035870b2861
Andrei Tatarnikov <andrewt@ispras.ru>
2014-02-17 10:51:51 +0000

    RepeatOperation was merged with RepeatOperationTestCase.

--- src/test/java/ru/ispras/fortress/solver/RepeatOperationTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/RepeatOperationTestCase.java
@@ -1,8 +1,22 @@
 package ru.ispras.fortress.solver;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.ispras.fortress.data.DataType;
+import ru.ispras.fortress.data.Variable;
+import ru.ispras.fortress.expression.NodeExpr;
+import ru.ispras.fortress.expression.NodeValue;
+import ru.ispras.fortress.expression.NodeVariable;
+import ru.ispras.fortress.expression.StandardOperation;
 import ru.ispras.fortress.solver.common.GenericSolverSampleTestBase;
+import ru.ispras.fortress.solver.constraint.Constraint;
+import ru.ispras.fortress.solver.constraint.ConstraintBuilder;
+import ru.ispras.fortress.solver.constraint.ConstraintKind;
+import ru.ispras.fortress.solver.constraint.Formulas;
 import ru.ispras.fortress.solver.samples.ISampleConstraint;
-import ru.ispras.fortress.solver.samples.RepeatOperation;
+import ru.ispras.fortress.transform.ReduceOptions;
+import ru.ispras.fortress.transform.Transform;
 
 public class RepeatOperationTestCase extends GenericSolverSampleTestBase
 {
@@ -10,5 +24,87 @@ public class RepeatOperationTestCase extends GenericSolverSampleTestBase
     public ISampleConstraint createSample()
     {
         return new RepeatOperation();
+    }
+
+    /** The constraint as described in the SMT language:
+
+    <pre>
+    (declare-const x (_ BitVec 4))
+    (declare-const y (_ BitVec 16))
+    (assert (= x #x5))
+    (assert (= y ((_ repeat 4) x)))
+    (check-sat)
+    (get-value (x y))
+    (exit)</pre>
+
+    Expected output: sat ((x #x5)(y #x5555))
+    */
+
+    public static class RepeatOperation implements ISampleConstraint
+    {
+        private static final int      BIT_VECTOR_ARG_SIZE = 4;
+        private static final DataType BIT_VECTOR_ARG_TYPE = DataType.BIT_VECTOR(BIT_VECTOR_ARG_SIZE);
+        private static final int      BIT_VECTOR_RES_SIZE = 16;
+        private static final DataType BIT_VECTOR_RES_TYPE = DataType.BIT_VECTOR(BIT_VECTOR_RES_SIZE);
+        private static final DataType INT_TYPE            = DataType.INTEGER;
+
+        private static final int HEX_RADIX = 16;
+        private static final int DEC_RADIX = 10;
+
+        @Override
+        public Constraint getConstraint()
+        {
+            final ConstraintBuilder builder = new ConstraintBuilder();
+
+            builder.setName("RepeatOperation");
+            builder.setKind(ConstraintKind.FORMULA_BASED);
+            builder.setDescription("RepeatOperation constraint");
+
+            final NodeVariable x = new NodeVariable(builder.addVariable("x", BIT_VECTOR_ARG_TYPE));
+            final NodeVariable y = new NodeVariable(builder.addVariable("y", BIT_VECTOR_RES_TYPE));
+
+            final Formulas formulas = new Formulas();
+            builder.setInnerRep(formulas);
+
+            formulas.add(
+                new NodeExpr(
+                    StandardOperation.EQ,
+                    x,
+                    new NodeValue(BIT_VECTOR_ARG_TYPE.valueOf("5", HEX_RADIX))
+                )
+            );
+
+            formulas.add(
+                new NodeExpr(
+                    StandardOperation.EQ,
+                    y,
+                    new NodeExpr(
+                        StandardOperation.BVREPEAT,
+                        Transform.reduce(
+                            ReduceOptions.NEW_INSTANCE,
+                            new NodeExpr(
+                                StandardOperation.ADD,
+                                new NodeValue(INT_TYPE.valueOf("2", DEC_RADIX)),
+                                new NodeValue(INT_TYPE.valueOf("2", DEC_RADIX))
+                            )
+                        ),
+                        x
+                    )
+                )
+            );
+
+            return builder.build();
+        }
+
+        @Override
+        public Iterable<Variable> getExpectedVariables()
+        {
+            final List<Variable> result = new ArrayList<Variable>();
+
+            result.add(new Variable("x", BIT_VECTOR_ARG_TYPE.valueOf("5", HEX_RADIX)));
+            result.add(new Variable("y", BIT_VECTOR_RES_TYPE.valueOf("5555", HEX_RADIX)));
+
+            return result;
+        }
     }
 }
