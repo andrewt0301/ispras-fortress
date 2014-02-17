ad67ee82986790f97bcebfb1290ef410d9a1cc25
Andrei Tatarnikov <andrewt@ispras.ru>
2014-02-17 12:46:59 +0000

    ConstraintDisjunction was merged with ConstraintDisjunctionTestCase.

--- src/test/java/ru/ispras/fortress/solver/ConstraintDisjunctionTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/ConstraintDisjunctionTestCase.java
@@ -1,7 +1,13 @@
 package ru.ispras.fortress.solver;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.ispras.fortress.data.Variable;
 import ru.ispras.fortress.solver.common.GenericSolverSampleTestBase;
-import ru.ispras.fortress.solver.samples.ConstraintDisjunction;
+import ru.ispras.fortress.solver.constraint.Constraint;
+import ru.ispras.fortress.solver.constraint.ConstraintCombiner;
+import ru.ispras.fortress.solver.samples.BVUGTOperation;
 import ru.ispras.fortress.solver.samples.ISampleConstraint;
 
 public class ConstraintDisjunctionTestCase extends GenericSolverSampleTestBase
@@ -10,5 +16,26 @@ public class ConstraintDisjunctionTestCase extends GenericSolverSampleTestBase
     public ISampleConstraint createSample()
     {
         return new ConstraintDisjunction();
+    }
+
+    public class ConstraintDisjunction extends BVUGTOperation
+    {
+        public Constraint getConstraint()
+        {
+            final Constraint constraint = super.getConstraint();
+            final Constraint        neg = ConstraintCombiner.makeNegation(constraint);
+
+            return ConstraintCombiner.makeDisjunction(constraint, neg);
+        }
+
+        @Override
+        public Iterable<Variable> getExpectedVariables()
+        {
+            final List<Variable> result = new ArrayList<Variable>();
+
+            result.add(new Variable("x", BIT_VECTOR_TYPE.valueOf("0", 10)));
+
+            return result;
+        }
     }
 }
