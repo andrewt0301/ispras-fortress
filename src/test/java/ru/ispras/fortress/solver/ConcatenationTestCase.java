0c049ea70f847cc2e25804a6e14bd37aabee6a5c
Andrei Tatarnikov <andrewt@ispras.ru>
2014-02-17 11:48:48 +0000

    Concatenation was merged with ConcatenationTestCase.

--- src/test/java/ru/ispras/fortress/solver/ConcatenationTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/ConcatenationTestCase.java
@@ -1,7 +1,19 @@
 package ru.ispras.fortress.solver;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.ispras.fortress.data.DataType;
+import ru.ispras.fortress.data.Variable;
+import ru.ispras.fortress.expression.NodeExpr;
+import ru.ispras.fortress.expression.NodeValue;
+import ru.ispras.fortress.expression.NodeVariable;
+import ru.ispras.fortress.expression.StandardOperation;
 import ru.ispras.fortress.solver.common.GenericSolverSampleTestBase;
-import ru.ispras.fortress.solver.samples.Concatenation;
+import ru.ispras.fortress.solver.constraint.Constraint;
+import ru.ispras.fortress.solver.constraint.ConstraintBuilder;
+import ru.ispras.fortress.solver.constraint.ConstraintKind;
+import ru.ispras.fortress.solver.constraint.Formulas;
 import ru.ispras.fortress.solver.samples.ISampleConstraint;
 
 public class ConcatenationTestCase extends GenericSolverSampleTestBase
@@ -10,5 +22,93 @@ public class ConcatenationTestCase extends GenericSolverSampleTestBase
     public ISampleConstraint createSample()
     {
         return new Concatenation();
+    }
+
+    /** The constraint as described in the SMT language:
+
+    <pre>
+    (declare-const x (_ BitVec 16))
+    (declare-const y (_ BitVec 16))
+    (declare-const z (_ BitVec 32))
+    (assert (= x #x0000))
+    (assert (= y #xffff))
+    (assert (= z (concat x y)))
+    (check-sat)
+    (get-value (x y z))
+    (exit)</pre>
+
+    Expected output: 
+
+    <pre>
+    sat ((z #x0000ffff))</pre>
+    */
+
+    public static class Concatenation implements ISampleConstraint
+    {
+        private static final int      BIT_VECTOR_ARG_SIZE = 16;
+        private static final DataType BIT_VECTOR_ARG_TYPE = DataType.BIT_VECTOR(BIT_VECTOR_ARG_SIZE);
+        private static final int      BIT_VECTOR_RES_SIZE = 32;
+        private static final DataType BIT_VECTOR_RES_TYPE = DataType.BIT_VECTOR(BIT_VECTOR_RES_SIZE);
+
+        private static final int HEX_RADIX = 16;
+
+        @Override
+        public Constraint getConstraint()
+        {
+            final ConstraintBuilder builder = new ConstraintBuilder();
+
+            builder.setName("Concatenation");
+            builder.setKind(ConstraintKind.FORMULA_BASED);
+            builder.setDescription("Concatenation constraint");
+
+            final NodeVariable x = new NodeVariable(builder.addVariable("x", BIT_VECTOR_ARG_TYPE));
+            final NodeVariable y = new NodeVariable(builder.addVariable("y", BIT_VECTOR_ARG_TYPE));
+            final NodeVariable z = new NodeVariable(builder.addVariable("z", BIT_VECTOR_RES_TYPE));
+
+            final Formulas formulas = new Formulas();
+            builder.setInnerRep(formulas);
+
+            formulas.add(
+                new NodeExpr(
+                    StandardOperation.EQ,
+                    x,
+                    new NodeValue(BIT_VECTOR_ARG_TYPE.valueOf("0000", HEX_RADIX))
+                )
+            );
+
+            formulas.add(
+                new NodeExpr(
+                    StandardOperation.EQ,
+                    y,
+                    new NodeValue(BIT_VECTOR_ARG_TYPE.valueOf("ffff", HEX_RADIX))
+                )
+            );
+
+            formulas.add(
+                new NodeExpr(
+                    StandardOperation.EQ,
+                    z,
+                    new NodeExpr(
+                        StandardOperation.BVCONCAT,
+                        x,
+                        y
+                    )
+                )
+            );
+
+            return builder.build();
+        }
+
+        @Override
+        public Iterable<Variable> getExpectedVariables()
+        {
+            final List<Variable> result = new ArrayList<Variable>();
+
+            result.add(new Variable("x", BIT_VECTOR_ARG_TYPE.valueOf("0000", HEX_RADIX)));
+            result.add(new Variable("y", BIT_VECTOR_ARG_TYPE.valueOf("ffff", HEX_RADIX)));
+            result.add(new Variable("z", BIT_VECTOR_RES_TYPE.valueOf("0000ffff", HEX_RADIX)));
+
+            return result;
+        }
     }
 }
