74df28538def6106a945be34672506bddd6d04f2
Andrei Tatarnikov <andrewt@ispras.ru>
2014-02-17 12:16:14 +0000

    NoNameArithmetic was merged with NoNameArithmeticTestCase.

--- src/test/java/ru/ispras/fortress/solver/NoNameArithmeticTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/NoNameArithmeticTestCase.java
@@ -13,11 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package ru.ispras.fortress.solver;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.ispras.fortress.data.DataType;
+import ru.ispras.fortress.data.Variable;
+import ru.ispras.fortress.expression.NodeExpr;
+import ru.ispras.fortress.expression.NodeValue;
+import ru.ispras.fortress.expression.NodeVariable;
+import ru.ispras.fortress.expression.StandardOperation;
 import ru.ispras.fortress.solver.common.GenericSolverSampleTestBase;
+import ru.ispras.fortress.solver.constraint.Constraint;
+import ru.ispras.fortress.solver.constraint.ConstraintBuilder;
+import ru.ispras.fortress.solver.constraint.Formulas;
 import ru.ispras.fortress.solver.samples.ISampleConstraint;
-import ru.ispras.fortress.solver.samples.NoNameArithmetic;
 
 /**
  * @author <a href="mailto:ssedai@ispras.ru">Sergey Smolov</a>
@@ -29,5 +41,60 @@ public class NoNameArithmeticTestCase extends GenericSolverSampleTestBase
     public ISampleConstraint createSample()
     {
         return new NoNameArithmetic();
+    }
+    
+    /** The constraint as described in the SMT language:
+
+    <pre>
+    (declare-const x Int)
+    (assert (= x (+ 2 2)))
+    (check-sat)
+    (get-value (x))
+    (exit)</pre>
+
+    Expected output: 
+    
+    <pre>
+    sat ((x 4))</pre>
+    */
+
+    public static class NoNameArithmetic implements ISampleConstraint
+    {
+        private static final DataType intType = DataType.INTEGER;
+
+        @Override
+        public Constraint getConstraint()
+        {
+            final ConstraintBuilder builder = new ConstraintBuilder();
+
+            final NodeVariable x = new NodeVariable(builder.addVariable("x", intType));
+
+            final Formulas formulas = new Formulas();
+            builder.setInnerRep(formulas);
+
+            formulas.add(
+                new NodeExpr(
+                    StandardOperation.EQ,
+                    x,
+                    new NodeExpr(
+                        StandardOperation.ADD,
+                        new NodeValue(intType.valueOf("2", 10)),
+                        new NodeValue(intType.valueOf("2", 10))
+                    )
+                )
+            );
+
+            return builder.build();
+        }
+
+        @Override
+        public Iterable<Variable> getExpectedVariables()
+        {
+            final List<Variable> result = new ArrayList<Variable>();
+
+            result.add(new Variable("x", intType.valueOf("4", 10)));
+
+            return result;
+        }
     }
 }
