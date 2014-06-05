d173631f255d595266b5474c9e3628b2c74d9fe3
Artem Kotsynyak <kotsynyak@ispras.ru>
2014-06-05 13:45:55 +0000

    test case for parsing string into KV-store

---
+++ src/test/java/ru/ispras/fortress/data/DataTypeTestCase.java
@@ -0,0 +1,28 @@
+package ru.ispras.fortress.data;
+
+import org.junit.*;
+import java.util.Map;
+
+public class DataTypeTestCase
+{
+    @Test
+    @SuppressWarnings("unchecked")
+    public void parseKeyValue()
+    {
+        final String strval = "((1:2)(2:3)(3:4))";
+        final DataType type = DataType.KV_STORE(DataType.INTEGER, DataType.INTEGER);
+        final Data data = type.valueOf(strval, 10);
+
+        Assert.assertTrue(data.getValue() instanceof Map);
+
+        final Map<Data, Data> map = (Map<Data, Data>) data.getValue();
+        Assert.assertTrue(map.size() == 3);
+
+        for (Map.Entry<Data, Data> entry : map.entrySet())
+        {
+            int key = (Integer) entry.getKey().getValue();
+            int value = (Integer) entry.getValue().getValue();
+            Assert.assertTrue(value == key + 1);
+        }
+    }
+}
