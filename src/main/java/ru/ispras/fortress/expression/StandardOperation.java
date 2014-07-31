ef4a60235ed03e02e96988b1a713fc8f05325f75
Andrei Tatarnikov <andrewt@ispras.ru>
2014-07-31 07:49:07 +0000

    Task #4692

--- src/main/java/ru/ispras/fortress/expression/StandardOperation.java
+++ src/main/java/ru/ispras/fortress/expression/StandardOperation.java
@@ -97,6 +97,19 @@ public enum StandardOperation
 
     /** Group: Logic, Operation: Power */
     POWER,
+    
+    /**
+    The items below belong to the "Logic Arithmetic (Additional)" group.
+    */
+
+    /** Group: Logic, Operation: Absolute value */
+    ABS,
+
+    /** Group: Logic, Operation: Minimum */
+    MIN,
+
+    /** Group: Logic, Operation: Maximum */
+    MAX,
 
     /**
     The items below belong to the "Basic Bitvector Arithmetic" group.
@@ -208,6 +221,43 @@ public enum StandardOperation
 
     /** Group: Bitvector, Operation: Signed greater than */
     BVSGT,
+
+    /**
+     * The items below belong to the "Bit Vector Reduction Operations" group.
+     * 
+     * Operation semantics:
+     * <pre>
+     * From IEEE Standard for Verilog Hardware Description Language:
+     *
+     * The unary reduction operators shall perform a bitwise operation on a single operand
+     * to produce a single-bit result. For reduction and, reduction or, and reduction xor operators,
+     * the first step of the operation shall apply the operator between the first bit of the operand
+     * and the second. The second and subsequent steps shall apply the operator between the 1-bit result
+     * of the prior step and the next bit of the operand using the same logic table. For reduction nand,
+     * reduction nor, and reduction xnor operators, the result shall be computed by inverting the result
+     * of the reduction and, reduction or, and reduction xor operation, respectively.
+     * 
+     * See the manual for details.
+     * </pre>
+     */
+
+    /** Group: Bit Vector Reduction, Operation: Reduction AND (&) */
+    BVANDR,
+
+    /** Group: Bit Vector Reduction, Operation: Reduction NAND (~&) */
+    BVNANDR,
+
+    /** Group: Bit Vector Reduction, Operation: Reduction OR (|) */
+    BVORR,
+
+    /** Group: Bit Vector Reduction, Operation: Reduction NOR (~|) */
+    BVNORR,
+    
+    /** Group: Bit Vector Reduction, Operation: Reduction XOR (^) */
+    BVXORR,
+    
+    /** Group: Bit Vector Reduction, Operation: Reduction XNOR (~^) */
+    BVXNORR,
 
     /**
     The items below belong to the "Array Operations" group.
