ac0f4b1e2ac67361290810a4129cd13aabcfdf43
Artem Kotsynyak <kotsynyak@ispras.ru>
2014-08-24 21:29:26 +0000

    javadoc for NodeBinding

--- src/main/java/ru/ispras/fortress/expression/NodeBinding.java
+++ src/main/java/ru/ispras/fortress/expression/NodeBinding.java
@@ -7,8 +7,17 @@ import java.util.Collections;
 
 import ru.ispras.fortress.data.DataType;
 
+/**
+ * The NodeBinding class represents set of substitutions to be considered
+ * in underlying expression subtree. List of bound variables considered
+ * immutable.
+ */
+
 public final class NodeBinding extends Node
 {
+    /**
+    * The BoundVariable class represents key-value binding pair.
+    */
     public final static class BoundVariable
     {
         private final NodeVariable variable;
@@ -20,10 +29,22 @@ public final class NodeBinding extends Node
             this.value = value;
         }
 
+        /**
+         * Returns object representing bound variable.
+         * 
+         *  @return A bound variable object reference.
+         */
+
         public NodeVariable getVariable()
         {
             return variable;
         }
+
+        /**
+         * Returns bound value object.
+         * 
+         *  @return A bound value.
+         */
 
         public Node getValue()
         {
@@ -57,17 +78,27 @@ public final class NodeBinding extends Node
     private Node expr;
     private List<BoundVariable> bindings;
 
-    public NodeBinding(Node expr, List<BoundVariable> bindings)
+    /**
+     * Creates a node based on expression and list of bindings.
+     * List of bound variables considered immutable therefore incurring
+     * need for copying input list. List is sorted because equality
+     * comparison relies on order of bindings.
+     *
+     * @param expression Expression subtree.
+     * @param bindings List of bound variables.
+     */
+
+    public NodeBinding(Node expression, List<BoundVariable> bindings)
     {
         super(Kind.BINDING);
 
-        if (expr == null)
+        if (expression == null)
             throw new NullPointerException();
 
         if (bindings == null)
             throw new NullPointerException();
 
-        this.expr = expr;
+        this.expr = expression;
 
         if (bindings.isEmpty())
             this.bindings = Collections.emptyList();
@@ -91,6 +122,13 @@ public final class NodeBinding extends Node
         Collections.sort(this.bindings, cmp);
     }
 
+    /**
+     * Constructor to avoid redundant allocations. Do not incur sorting.
+     * 
+     * @param expr Expression subtree.
+     * @param bindings List of bound variables.
+     */
+
     private NodeBinding(Node expr, List<BoundVariable> bindings, int unused)
     {
         super(Kind.BINDING);
@@ -99,25 +137,59 @@ public final class NodeBinding extends Node
         this.bindings = bindings;
     }
 
+    /**
+     * Returns list of bound variables associated with the node.
+     * 
+     * @return Unmodifiable list of bound variables.
+     */
+
     public List<BoundVariable> getBindings()
     {
         return Collections.unmodifiableList(bindings);
     }
+
+    /**
+     * Returns underlying expression subtree.
+     * 
+     * @return An expression.
+     */
 
     public Node getExpression()
     {
         return expr;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+
     public Node deepCopy()
     {
         return new NodeBinding(this.expr, this.bindings, 0);
     }
 
-    public NodeBinding bindTo(Node expr)
+    /**
+     * Create binding node with same bindings for given expression.
+     * Avoids additional costs of using constructor directly.
+     * 
+     * @param expression An expression subtree.
+     *
+     * @return A binding node object.
+     */
+
+    public NodeBinding bindTo(Node expression)
     {
-        return new NodeBinding(expr, this.bindings, 0);
+        return new NodeBinding(expression, this.bindings, 0);
     }
+
+    /**
+     * Create bound variable using NodeVariable object and expression.
+     * 
+     * @param variable An object representing bound variable.
+     * @param value A bound value.
+     *
+     * @return A bound variable object.
+     */
 
     public static BoundVariable bindVariable(NodeVariable variable, Node value)
     {
@@ -128,6 +200,10 @@ public final class NodeBinding extends Node
 
         return new BoundVariable(variable, value);
     }
+
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public DataType getDataType()
