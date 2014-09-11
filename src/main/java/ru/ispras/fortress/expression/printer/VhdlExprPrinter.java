18ce5103c875ba18eec271dee117fe39d266b841
Sergey Smolov <ssedai@ispras.ru>
2014-09-11 08:12:17 +0000

    VhdlExprPrinter: brought in consistent state with VerilogExprPrinter

--- src/main/java/ru/ispras/fortress/expression/printer/VhdlExprPrinter.java
+++ src/main/java/ru/ispras/fortress/expression/printer/VhdlExprPrinter.java
@@ -41,36 +41,74 @@ final class VhdlExprPrinter extends MapBasedPrinter
 
     public VhdlExprPrinter()
     {
+        // Supported operations.
         addMapping(StandardOperation.MINUS,     "-",   Type.PREFIX);
         addMapping(StandardOperation.PLUS,      "+",   Type.PREFIX);
-        addMapping(StandardOperation.NOT,       "not", Type.PREFIX);
-        addMapping(StandardOperation.BVNOT,     "not", Type.PREFIX);
+        addMapping(StandardOperation.NOT,       "NOT", Type.PREFIX);
+        addMapping(StandardOperation.BVNOT,     "NOT", Type.PREFIX); // BVNOT = NOT
         addMapping(StandardOperation.POWER,     "**",  Type.INFIX);
         addMapping(StandardOperation.MUL,       "*",   Type.INFIX);
+        addMapping(StandardOperation.BVMUL,     "*",   Type.INFIX); // BVMUL = MUL
         addMapping(StandardOperation.DIV,       "/",   Type.INFIX);
-        addMapping(StandardOperation.MOD,       "mod", Type.INFIX);
+        addMapping(StandardOperation.MOD,       "MOD", Type.INFIX);
+        addMapping(StandardOperation.BVSMOD,    "MOD", Type.INFIX); // BVSMOD = MOD
         addMapping(StandardOperation.ADD,       "+",   Type.INFIX);
+        addMapping(StandardOperation.BVADD,     "+",   Type.INFIX); // BVADD = ADD
         addMapping(StandardOperation.SUB,       "-",   Type.INFIX);
-        addMapping(StandardOperation.BVLSHR,    "srl", Type.INFIX);
-        addMapping(StandardOperation.BVLSHL,    "sll", Type.INFIX);
-        addMapping(StandardOperation.BVASHR,    "sra", Type.INFIX);
-        addMapping(StandardOperation.BVASHL,    "sla", Type.INFIX);
-        addMapping(StandardOperation.BVROL,     "rol", Type.INFIX);
-        addMapping(StandardOperation.BVROR,     "ror", Type.INFIX);
+        addMapping(StandardOperation.BVSUB,     "-",   Type.INFIX); // BVSUB = SUB
+        addMapping(StandardOperation.BVLSHR,    "SRL", Type.INFIX);
+        addMapping(StandardOperation.BVLSHL,    "SLL", Type.INFIX);
+        addMapping(StandardOperation.BVASHR,    "SRA", Type.INFIX);
+        addMapping(StandardOperation.BVASHL,    "SLA", Type.INFIX);
+        addMapping(StandardOperation.BVROR,     "ROR", Type.INFIX);
+        addMapping(StandardOperation.BVROL,     "ROL", Type.INFIX);
         addMapping(StandardOperation.LESS,      "<",   Type.INFIX);
+        addMapping(StandardOperation.BVULT,     "<",   Type.INFIX); // BVULT = LESS
+        addMapping(StandardOperation.BVSLT,     "<",   Type.INFIX); // BVSLT = LESS
         addMapping(StandardOperation.LESSEQ,    "<=",  Type.INFIX);
+        addMapping(StandardOperation.BVULE,     "<=",  Type.INFIX); // BVULE = LESSEQ
+        addMapping(StandardOperation.BVSLE,     "<=",  Type.INFIX); // BVSLE = LESSEQ
         addMapping(StandardOperation.GREATER,   ">",   Type.INFIX);
+        addMapping(StandardOperation.BVUGT,     ">",   Type.INFIX); // BVUGT = GREATER
+        addMapping(StandardOperation.BVSGT,     ">",   Type.INFIX); // BVSGT = GREATER
         addMapping(StandardOperation.GREATEREQ, ">=",  Type.INFIX);
+        addMapping(StandardOperation.BVUGE,     ">=",  Type.INFIX); // BVUGE = GREATEREQ
+        addMapping(StandardOperation.BVSGE,     ">=",  Type.INFIX); // BVSGE = GREATEREQ
         addMapping(StandardOperation.EQ,        "=",   Type.INFIX);
         addMapping(StandardOperation.NOTEQ,     "/=",  Type.INFIX);
-        addMapping(StandardOperation.EQCASE,    "=",   Type.INFIX);
-        addMapping(StandardOperation.NOTEQCASE, "/=",  Type.INFIX);
-        addMapping(StandardOperation.BVAND,     "and", Type.INFIX);
-        addMapping(StandardOperation.BVXOR,     "xor", Type.INFIX);
-        addMapping(StandardOperation.BVOR,      "or",  Type.INFIX);
-        addMapping(StandardOperation.AND,       "and", Type.INFIX);
-        addMapping(StandardOperation.OR,        "or",  Type.INFIX);
+        addMapping(StandardOperation.EQCASE,    "=",   Type.INFIX); // EQCASE = EQ
+        addMapping(StandardOperation.NOTEQCASE, "/=",  Type.INFIX); // NOTEQCASE = NOTEQ
+        addMapping(StandardOperation.AND,       "AND", Type.INFIX);
+        addMapping(StandardOperation.BVAND,     "AND", Type.INFIX); // BVAND = AND
+        addMapping(StandardOperation.OR,        "OR",  Type.INFIX);
+        addMapping(StandardOperation.BVOR,      "OR",  Type.INFIX); // BVOR = OR
+        addMapping(StandardOperation.BVXOR,     "XOR", Type.INFIX);
+        addMapping(StandardOperation.BVNOR,     "NOR", Type.INFIX);
         addMapping(StandardOperation.BVCONCAT,  "&",   Type.INFIX);
+        addMapping(StandardOperation.MIN,       "MINIMUM(", ", ", ")");
+        addMapping(StandardOperation.MAX,       "MAXIMUM(", ", ", ")");
         addMapping(StandardOperation.ITE,       new String[] { "?", ":" });
+
+        // Unsupported operations.
+        addMapping(StandardOperation.BVNAND,    "BVNAND(", ", ", ")");
+        addMapping(StandardOperation.BVXNOR,    "BVZNOR(", ", ", ")");
+        addMapping(StandardOperation.BVANDR,    "BVANDR(", ", ", ")");
+        addMapping(StandardOperation.BVNANDR,   "BVNANDR(", ", ", ")");
+        addMapping(StandardOperation.BVORR,     "BVORR(", ", ", ")");
+        addMapping(StandardOperation.BVNORR,    "BVNORR(", ", ", ")");
+        addMapping(StandardOperation.BVXORR,    "BVXORR(", ", ", ")");
+        addMapping(StandardOperation.BVXNORR,   "BVXNORR(", ", ", ")");
+        addMapping(StandardOperation.BVREPEAT,  "BVREPEAT(", ", ", ")");
+        addMapping(StandardOperation.BVEXTRACT, "BVEXTRACT(", ", ", ")");
+
+        addMapping(StandardOperation.BVUREM,    "BVUREM(", ", ", ")");
+        addMapping(StandardOperation.BVSREM,    "BVSREM(", ", ", ")");
+        addMapping(StandardOperation.BVZEROEXT, "BVZEROEXT(", ", ", ")");
+        addMapping(StandardOperation.BVSIGNEXT, "BVSIGNEXT(", ", ", ")");
+        addMapping(StandardOperation.REM,       "REM(", ", ", ")");
+        addMapping(StandardOperation.ABS,       "ABS(", ", ", ")");
+        addMapping(StandardOperation.IMPL,      "IMPL(", ", ", ")");
+        addMapping(StandardOperation.SELECT,    "SELECT(", ", ", ")");
+        addMapping(StandardOperation.STORE,     "STORE(", ", ", ")");
     }
 }
