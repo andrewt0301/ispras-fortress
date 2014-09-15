0afcc0e88f77b363fa62d62090d72a011f7c305a
Andrei Tatarnikov <andrewt@ispras.ru>
2014-09-15 11:02:07 +0000

    Added header.

--- src/main/java/ru/ispras/fortress/expression/NodeBinding.java
+++ src/main/java/ru/ispras/fortress/expression/NodeBinding.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2014 ISPRAS (www.ispras.ru)
+ * 
+ * Institute for System Programming of Russian Academy of Sciences
+ * 
+ * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * 
+ * All rights reserved.
+ * 
+ * NodeBinding.java, Sep 15, 2014 3:05:05 PM Artem Kotsynyak
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package ru.ispras.fortress.expression;
 
 import java.util.Arrays;
