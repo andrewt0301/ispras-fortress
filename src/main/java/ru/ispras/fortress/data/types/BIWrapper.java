96edae11d371707c19dc893ad9a65816ef071495
Andrei Tatarnikov <andrewt@ispras.ru>
2014-11-05 15:11:40 +0000

    Google code style for ru.ispras.fortress.data.types

--- src/main/java/ru/ispras/fortress/data/types/BIWrapper.java
+++ src/main/java/ru/ispras/fortress/data/types/BIWrapper.java
@@ -1,13 +1,15 @@
 /*
- * Copyright (c) 2012 ISPRAS
+ * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
  * 
- * Institute for System Programming of Russian Academy of Sciences
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * All rights reserved.
- * 
- * BIWrapper.java, May 5, 2012 6:08:16 PM Andrei Tatarnikov
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package ru.ispras.fortress.data.types;
@@ -15,165 +17,171 @@ package ru.ispras.fortress.data.types;
 import java.math.BigInteger;
 
 /**
- * The BIWrapper class provides a wrapper around the BigInteger class
- * to conveniently store and convert binary data. 
+ * The BIWrapper class provides a wrapper around the BigInteger class to conveniently store and
+ * convert binary data.
  * 
  * @author Andrei Tatarnikov
  */
 
 @Deprecated
-public final class BIWrapper
-{
-    /**
-     * The BITS_IN_HEX constant specifies the number of bit a buffer should contain 
-     * to be represented by one hexadecimal character (a hexadecimal number of length 1).  
-     */
+public final class BIWrapper {
+  /**
+   * The BITS_IN_HEX constant specifies the number of bit a buffer should contain to be represented
+   * by one hexadecimal character (a hexadecimal number of length 1).
+   */
 
-    private static final int BITS_IN_HEX = 4;
+  private static final int BITS_IN_HEX = 4;
 
-    private final BigInteger value;
-    private final int        size;
-    private final int        radix;
+  private final BigInteger value;
+  private final int size;
+  private final int radix;
 
-    /**
-     * Constructs a BIWrapper object from a BigInteger value.  
-     * 
-     * @param value A BitVectorWrapper object that stores data.
-     * @param size  The size of the data in bits.
-     * @param radix The radix that will be used in data conversions.
-     */
+  /**
+   * Constructs a BIWrapper object from a BigInteger value.
+   * 
+   * @param value A BitVectorWrapper object that stores data.
+   * @param size The size of the data in bits.
+   * @param radix The radix that will be used in data conversions.
+   */
 
-    public BIWrapper(BigInteger value, int size, int radix)
-    {
-        assert null != value; 
-
-        this.value = value;
-        this.size  = size;
-        this.radix = radix;
+  public BIWrapper(BigInteger value, int size, int radix) {
+    if (null == value) {
+      throw new NullPointerException();
     }
 
-    /**
-     * A factory method that creates a BIWrapper object from
-     * a string representation. 
-     *  
-     * @param value The string representation of the data.
-     * @param radix The radix that will be used in to convert the string to a BigInteger object.
-     * @param size  The size of the data in bits.
-     * @param typeRadix The radix that will be used to convert the data object to string.
-     * @return object of BitVectorWrapper
-     */
+    this.value = value;
+    this.size = size;
+    this.radix = radix;
+  }
 
-    public static BIWrapper valueOf(String value, int radix, int size, int typeRadix)
-    {
-        return new BIWrapper(new BigInteger(value, radix), size, typeRadix);
+  /**
+   * A factory method that creates a BIWrapper object from a string representation.
+   * 
+   * @param value The string representation of the data.
+   * @param radix The radix that will be used in to convert the string to a BigInteger object.
+   * @param size The size of the data in bits.
+   * @param typeRadix The radix that will be used to convert the data object to string.
+   * @return object of BitVectorWrapper
+   */
+
+  public static BIWrapper valueOf(String value, int radix, int size, int typeRadix) {
+    return new BIWrapper(new BigInteger(value, radix), size, typeRadix);
+  }
+
+  /**
+   * Converts the stored data into a binary string.
+   * 
+   * @return A string representing the stored value in binary format.
+   */
+
+  public String toBinString() {
+    final String binstr = value.toString(Radix.BIN.value());
+    final int count = size - binstr.length();
+
+    return (count > 0) ? String.format("%0" + count + "d", 0) + binstr : binstr;
+  }
+
+  /**
+   * Converts the stored data into a hexadecimal string.
+   * 
+   * @return A string representing the stored value in hexadecimal format.
+   */
+
+  public String toHexString() {
+    final String hexstr = value.toString(Radix.HEX.value());
+    final int count = size / BITS_IN_HEX - hexstr.length();
+
+    return (count > 0) ? String.format("%0" + count + "d", 0) + hexstr : hexstr;
+  }
+
+  /**
+   * Converts the stored data to a string (the string format depends on radix).
+   * 
+   * @return The string representation of the stored data.
+   */
+
+  @Override
+  public String toString() {
+    if (radix == Radix.HEX.value()) {
+      return toHexString();
     }
 
-    /**
-     * Converts the stored data into a binary string. 
-     * @return A string representing the stored value in binary format.
-     */
-
-    public String toBinString()
-    {
-        final String binstr = value.toString(Radix.BIN.value());
-        final int     count = size - binstr.length();
-
-        return (count > 0) ? String.format("%0" + count + "d", 0) + binstr: binstr;
+    if (radix == Radix.BIN.value()) {
+      return toBinString();
     }
 
-    /**
-     * Converts the stored data into a hexadecimal string. 
-     * @return A string representing the stored value in hexadecimal format.
-     */
+    assert false;
+    return value.toString();
+  }
 
-    public String toHexString()
-    {
-        final String hexstr = value.toString(Radix.HEX.value());
-        final int     count = size / BITS_IN_HEX - hexstr.length();
+  /**
+   * Returns a BigInteger object which is the internal representation of the stored data.
+   * 
+   * @return A BigInteger object that holds the data (the internal representation).
+   */
 
-        return (count > 0) ? String.format("%0" + count + "d", 0) + hexstr: hexstr;
+  public BigInteger getValue() {
+    return value;
+  }
+
+  /**
+   * Returns the size of the stored data in bits.
+   * 
+   * @return Data size in bits.
+   */
+
+  public int getSize() {
+    return size;
+  }
+
+  /**
+   * Returns the radix that is used to translate data to/from a string.
+   * 
+   * @return The radix value.
+   */
+
+  public int getRadix() {
+    return radix;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+
+    result = prime * result + size;
+    result = prime * result + value.hashCode();
+
+    return result;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
     }
 
-    /**
-     * Converts the stored data to a string (the string format depends on radix).
-     * @return The string representation of the stored data. 
-     */
-
-    @Override
-    public String toString()
-    {
-        if (radix == Radix.HEX.value())
-            return toHexString();
-
-        if (radix == Radix.BIN.value())
-            return toBinString();
-
-        assert false;
-        return value.toString();
+    if (obj == null) {
+      return false;
     }
 
-    /**
-     * Returns a BigInteger object which is the internal representation of the stored data.  
-     * @return A BigInteger object that holds the data (the internal representation).
-     */
-
-    public BigInteger getValue()
-    {
-        return value;
+    if (getClass() != obj.getClass()) {
+      return false;
     }
 
-    /**
-     * Returns the size of the stored data in bits.
-     * @return Data size in bits.
-     */
-
-    public int getSize()
-    {
-        return size;
+    final BIWrapper other = (BIWrapper) obj;
+    if (size != other.size) {
+      return false;
     }
 
-    /**
-     * Returns the radix that is used to translate data to/from a string. 
-     * @return The radix value.
-     */
-
-    public int getRadix()
-    {
-        return radix;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-
-    @Override
-    public int hashCode()
-    {
-        final int prime = 31;
-        int result = 1;
-
-        result = prime * result + size;
-        result = prime * result + value.hashCode();
-
-        return result;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-
-    @Override
-    public boolean equals(Object obj)
-    {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (getClass() != obj.getClass()) return false;
-
-        final BIWrapper other = (BIWrapper) obj;
-
-        if (size != other.size)
-            return false;
-
-        return value.equals(other.value);
-    }
+    return value.equals(other.value);
+  }
 }
--- src/main/java/ru/ispras/fortress/data/types/Radix.java
+++ src/main/java/ru/ispras/fortress/data/types/Radix.java
@@ -1,44 +1,42 @@
 /*
- * Copyright (c) 2013 ISPRAS
+ * Copyright 2013-2014 ISP RAS (http://www.ispras.ru)
  * 
- * Institute for System Programming of Russian Academy of Sciences
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * All rights reserved.
- * 
- * Radix.java, Nov 5, 2013 3:56:13 PM Andrei Tatarnikov
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package ru.ispras.fortress.data.types;
 
 /**
- * The Radix enumeration describes radixes to be used to convert
- * a value to a string or vice versa.
+ * The Radix enumeration describes radixes to be used to convert a value to a string or vice versa.
  * 
  * @author Andrei Tatarnikov
  */
 
-public enum Radix
-{
-    /** Radix for binary data.*/
-    BIN(2),
+public enum Radix {
+  /** Radix for binary data. */
+  BIN(2),
 
-    /** Radix for decimal data.*/
-    DEC(10),
+  /** Radix for decimal data. */
+  DEC(10),
 
-    /** Radix for hexadecimal data.*/
-    HEX(16);
+  /** Radix for hexadecimal data. */
+  HEX(16);
 
-    private final int value;
+  private final int value;
 
-    private Radix(int value)
-    {
-        this.value = value;
-    }
+  private Radix(int value) {
+    this.value = value;
+  }
 
-    public int value()
-    {
-        return value;
-    }
+  public int value() {
+    return value;
+  }
 }
