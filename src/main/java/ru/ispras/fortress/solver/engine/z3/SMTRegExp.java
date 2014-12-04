4eb7adf5f24f244c38e9c8742aa08889cb4d2f64
Andrei Tatarnikov <andrewt@ispras.ru>
2014-12-04 07:41:25 +0000

    Bug #5460 - removed unsused constants from SMTRegExp.

--- src/main/java/ru/ispras/fortress/solver/engine/z3/SMTRegExp.java
+++ src/main/java/ru/ispras/fortress/solver/engine/z3/SMTRegExp.java
@@ -20,32 +20,14 @@ public final class SMTRegExp {
   public static final String SAT = "sat";
   public static final String UNSAT = "unsat";
   public static final String UNKNOWN = "unknown";
-  
+
   public static final String TRUE = "true";
   public static final String FALSE = "false";
-  
-  public static final String STATUS_PTRN = String.format("^(%s|%s|%s)$", SAT, UNSAT, UNKNOWN);
-
-  public static final String ERR_START = "^[(]error[ ][\"]";
-  public static final String ERR_END = "[\"][)]$";
-  public static final String ERR_PTRN = ERR_START + ".{0,}" + ERR_END;
-  public static final String ERR_TRIM_PTRN = String.format("(%s)|(%s)", ERR_START, ERR_END);
 
   public static final String VALUE_BIN = "[#][b][0|1]{1,}";
   public static final String VALUE_HEX = "[#][x][\\d|a-f|A-F]{1,}";
   public static final String VALUE_DEC = "\\(?(-\\s)?\\d+(\\.\\d+)?\\)?";
-  
-  public static final String ARRAY_REF = "_[ ]as-array[ ]([^ ]+)";
-  
-  public static final String VALUE_PTRN = String.format(
-    "((%s)|(%s)|(%s)|(%s)|(%s)|(\\(%s\\)))", TRUE, FALSE, VALUE_BIN, VALUE_HEX, VALUE_DEC, ARRAY_REF);
 
   public static final String VALUE_TRIM_PTRN = "^([#][b|x]){0,1}";
-  
-  public static final String EXPR_START = "^[ ]{0,}[(]{1,2}";
-  public static final String EXPR_END = "[)]{1,3}$";
-  public static final String EXPR_PTRN_FRMT = EXPR_START + "%s[ ]" + VALUE_PTRN + EXPR_END;
-  public static final String EXPR_TRIM_PTRN_FRMT = "(" + EXPR_START + "%s[ ][\\(]?)|(" + EXPR_END + ")";
-
   public static final String LINE_START = "^";
 }
