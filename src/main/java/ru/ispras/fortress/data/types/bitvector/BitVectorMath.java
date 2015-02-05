81dcd182a4ca3868e838f2634d7b452d6e4d81ad
Andrei Tatarnikov <andrewt@ispras.ru>
2015-02-05 13:10:35 +0000

    Updated BitVector math

--- src/main/java/ru/ispras/fortress/data/types/bitvector/BitVectorMath.java
+++ src/main/java/ru/ispras/fortress/data/types/bitvector/BitVectorMath.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 ISP RAS (http://www.ispras.ru)
+ * Copyright 2014-2015 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -14,6 +14,7 @@
 
 package ru.ispras.fortress.data.types.bitvector;
 
+import static ru.ispras.fortress.util.InvariantChecks.checkNotNull;
 import static ru.ispras.fortress.data.types.bitvector.BitVectorMath.Operands.*;
 
 public final class BitVectorMath {
@@ -574,12 +575,6 @@ public final class BitVectorMath {
     BitVectorAlgorithm.transform(v, result, op);
 
     return result;
-  }
-
-  private static void checkNotNull(Object o) {
-    if (null == o) {
-      throw new NullPointerException();
-    }
   }
 
   private static void checkEqualSize(BitVector lhs, BitVector rhs) {
