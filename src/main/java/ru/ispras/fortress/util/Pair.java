646366913c333f0385e34d348c7512aeaec4929d
Andrei Tatarnikov <andrewt@ispras.ru>
2015-03-18 15:15:08 +0000

    Added the Pair utility class.

---
+++ src/main/java/ru/ispras/fortress/util/Pair.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.fortress.util;
+
+import static ru.ispras.fortress.util.InvariantChecks.checkNotNull;
+
+public final class Pair<T, U> {
+  public final T first;
+  public final U second;
+
+  public Pair(T first, U second) {
+    checkNotNull(first);
+    checkNotNull(second);
+
+    this.first = first;
+    this.second = second;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+
+    if (obj == null) {
+      return false;
+    }
+
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+
+    final Pair<?, ?> other = (Pair<?, ?>) obj;
+    return first.equals(other.first) && second.equals(other.second);
+  }
+
+  @Override
+  public int hashCode() {
+    return 31 * first.hashCode() + second.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return String.format("<%s, %s>", first, second);
+  }
+}
