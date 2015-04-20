d95deb7c05d867fb2ebad3c8f6647b8083510f58
Andrei Tatarnikov <andrewt@ispras.ru>
2015-04-20 08:11:14 +0000

    Fixing PMD warnings.

--- src/main/java/ru/ispras/fortress/solver/engine/smt/SMTStrings.java
+++ src/main/java/ru/ispras/fortress/solver/engine/smt/SMTStrings.java
@@ -26,50 +26,50 @@ import ru.ispras.fortress.data.types.bitvector.BitVector;
 public final class SMTStrings {
   private SMTStrings() {}
 
-  public static final String sSPACE = " ";
-  public static final String sBRACKET_OPEN = "(";
-  public static final String sBRACKET_CLOSE = ")";
-  public static final String sHYPHEN = "-";
-  public static final String sUNDERLINE = "_";
+  public static final String SPACE = " ";
+  public static final String BRACKET_OPEN = "(";
+  public static final String BRACKET_CLOSE = ")";
+  public static final String HYPHEN = "-";
+  public static final String UNDERLINE = "_";
 
-  public static final String sZERO = "0";
-  public static final String sONE = "1";
+  public static final String ZERO = "0";
+  public static final String ONE = "1";
 
-  public static final String sEMPTY = "";
-  public static final String sTRUE = "true";
-  public static final String sFALSE = "false";
-  public static final String sDEFAULT_ARRAY = "DefaultArrayLiteral!%d";
+  public static final String EMPTY = "";
+  public static final String TRUE = "true";
+  public static final String FALSE = "false";
+  public static final String DEFAULT_ARRAY = "DefaultArrayLiteral!%d";
 
-  public static final String sBV_BIN_PREFIX = "#b";
-  public static final String sBV_HEX_PREFIX = "#x";
+  public static final String BV_BIN_PREFIX = "#b";
+  public static final String BV_HEX_PREFIX = "#x";
 
-  public static final String sTYPE_BOOL = "Bool";
-  public static final String sTYPE_INT = "Int";
-  public static final String sTYPE_REAL = "Real";
-  public static final String sTYPE_BITVECTOR = "(_ BitVec %d)";
-  public static final String sTYPE_ARRAY = "(Array %s %s)";
+  public static final String TYPE_BOOL = "Bool";
+  public static final String TYPE_INT = "Int";
+  public static final String TYPE_REAL = "Real";
+  public static final String TYPE_BITVECTOR = "(_ BitVec %d)";
+  public static final String TYPE_ARRAY = "(Array %s %s)";
 
-  public static final String sASSERT = "(assert %s)%n";
-  public static final String sDECLARE_CONST = "(declare-const %s %s)%n";
-  public static final String sDEFINE_FUN = "(define-fun %s)%n";
-  public static final String sCHECK_SAT = "(check-sat)";
-  public static final String sGET_VALUE = "(get-value (%s))%n";
-  public static final String sGET_MODEL = "(get-model)";
-  public static final String sEXIT = "(exit)";
+  public static final String ASSERT = "(assert %s)%n";
+  public static final String DECLARE_CONST = "(declare-const %s %s)%n";
+  public static final String DEFINE_FUN = "(define-fun %s)%n";
+  public static final String CHECK_SAT = "(check-sat)";
+  public static final String GET_VALUE = "(get-value (%s))%n";
+  public static final String GET_MODEL = "(get-model)";
+  public static final String EXIT = "(exit)";
 
-  public static final String sNEGATION = "(- %s)";
-  public static final String sPARAM_DEF = "(%s %s)";
+  public static final String NEGATION = "(- %s)";
+  public static final String PARAM_DEF = "(%s %s)";
 
   private static final Map<DataTypeId, String> typeMap = createTypeMap();
 
   private static Map<DataTypeId, String> createTypeMap() {
     final Map<DataTypeId, String> result = new EnumMap<DataTypeId, String>(DataTypeId.class);
 
-    result.put(DataTypeId.BIT_VECTOR, sTYPE_BITVECTOR);
-    result.put(DataTypeId.LOGIC_BOOLEAN, sTYPE_BOOL);
-    result.put(DataTypeId.LOGIC_INTEGER, sTYPE_INT);
-    result.put(DataTypeId.LOGIC_REAL, sTYPE_REAL);
-    result.put(DataTypeId.MAP, sTYPE_ARRAY);
+    result.put(DataTypeId.BIT_VECTOR, TYPE_BITVECTOR);
+    result.put(DataTypeId.LOGIC_BOOLEAN, TYPE_BOOL);
+    result.put(DataTypeId.LOGIC_INTEGER, TYPE_INT);
+    result.put(DataTypeId.LOGIC_REAL, TYPE_REAL);
+    result.put(DataTypeId.MAP, TYPE_ARRAY);
 
     return result;
   }
@@ -102,23 +102,23 @@ public final class SMTStrings {
       case BIT_VECTOR: {
         final BitVector value = (BitVector) data.getValue();
         return (data.getType().getTypeRadix() == 16) ?
-          sBV_HEX_PREFIX + value.toHexString() : sBV_BIN_PREFIX + value.toBinString();
+          BV_HEX_PREFIX + value.toHexString() : BV_BIN_PREFIX + value.toBinString();
       }
 
       case LOGIC_BOOLEAN: {
         final Boolean value = (Boolean) data.getValue();
-        return value ? sTRUE : sFALSE;
+        return value ? TRUE : FALSE;
       }
 
       case LOGIC_INTEGER: {
         final BigInteger value = (BigInteger) data.getValue();
         return (value.compareTo(BigInteger.ZERO) >= 0) ?
-          value.toString() : String.format(sNEGATION, value.abs());
+          value.toString() : String.format(NEGATION, value.abs());
       }
 
       case LOGIC_REAL: {
         final double value = (Double) data.getValue();
-        return (value >= 0) ? Double.toString(value) : String.format(sNEGATION, Math.abs(value));
+        return (value >= 0) ? Double.toString(value) : String.format(NEGATION, Math.abs(value));
       }
 
       case MAP: {
@@ -128,11 +128,11 @@ public final class SMTStrings {
 
         final String prefix = "(store ";
 
-        builder.ensureCapacity(prefix.length() * map.size() + sDEFAULT_ARRAY.length());
+        builder.ensureCapacity(prefix.length() * map.size() + DEFAULT_ARRAY.length());
         for (int i = 0; i < map.size(); ++i) {
           builder.append(prefix);
         }
-        builder.append(sDEFAULT_ARRAY).append(sSPACE);
+        builder.append(DEFAULT_ARRAY).append(SPACE);
 
         for (Map.Entry<?, ?> entry : map.entrySet()) {
           builder.append(textForData((Data) entry.getKey())).append(" ")
