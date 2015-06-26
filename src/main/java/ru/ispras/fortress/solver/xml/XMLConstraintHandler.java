c8a3c6f2561a9634f48911552bdd0d0b14a1f7f1
Andrei Tatarnikov <andrewt@ispras.ru>
2015-06-26 15:33:05 +0000

    Small code improvements

--- src/main/java/ru/ispras/fortress/solver/xml/XMLConstraintHandler.java
+++ src/main/java/ru/ispras/fortress/solver/xml/XMLConstraintHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 ISP RAS (http://www.ispras.ru)
+ * Copyright 2014-2015 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -42,26 +42,26 @@ import ru.ispras.fortress.solver.constraint.Formulas;
  * The XMLConstraintHandler class is handler for SAX parser events that builds a constraint basing
  * on its XML representation.
  * 
- * @author Andrei Tatarnikov
+ * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
  */
 
 final class XMLConstraintHandler extends DefaultHandler {
   private final XMLConstraintBuilder builder = new XMLConstraintBuilder();
-  private final Stack<XMLNodeType> nodes = new Stack<XMLNodeType>();
-  private final Map<String, Variable> variables = new HashMap<String, Variable>();
+  private final Stack<XMLNodeType> nodes = new Stack<>();
+  private final Map<String, Variable> variables = new HashMap<>();
 
-  private final Map<String, Variable> incompleteScope = new HashMap<String, Variable>();
+  private final Map<String, Variable> incompleteScope = new HashMap<>();
   private VariableScope nestedScope = VariableScope.EMPTY_SCOPE;
 
   private static class VariableScope {
     public final static VariableScope EMPTY_SCOPE = new VariableScope() {
       @Override
-      public boolean contains(String name) {
+      public boolean contains(final String name) {
         return false;
       }
 
       @Override
-      public Variable get(String name) {
+      public Variable get(final String name) {
         return null;
       }
     };
@@ -74,7 +74,9 @@ final class XMLConstraintHandler extends DefaultHandler {
       this.hidden = null;
     }
 
-    public VariableScope(VariableScope previous, Map<String, Variable> variables) {
+    public VariableScope(
+        final VariableScope previous,
+        final Map<String, Variable> variables) {
       this.variables = variables;
       this.hidden = previous;
     }
@@ -83,7 +85,7 @@ final class XMLConstraintHandler extends DefaultHandler {
       return hidden;
     }
 
-    public boolean contains(String name) {
+    public boolean contains(final String name) {
       if (variables.containsKey(name)) {
         return true;
       }
@@ -91,7 +93,7 @@ final class XMLConstraintHandler extends DefaultHandler {
       return hidden.contains(name);
     }
 
-    public Variable get(String name) {
+    public Variable get(final String name) {
       if (variables.containsKey(name)) {
         return variables.get(name);
       }
@@ -105,8 +107,11 @@ final class XMLConstraintHandler extends DefaultHandler {
   }
 
   @Override
-  public void startElement(String uri, String localName, String qName, Attributes attributes)
-      throws SAXException {
+  public void startElement(
+      final String uri,
+      final String localName,
+      final String qName,
+      final Attributes attributes) throws SAXException {
     try {
       final XMLNodeType currentNode = getNodeType(qName);
       verifyNodeHierarchy(currentNode, nodes.empty() ? null : nodes.lastElement());
@@ -211,12 +216,15 @@ final class XMLConstraintHandler extends DefaultHandler {
     // }
   }
 
-  private Variable createLocalVariable(String name) {
+  private Variable createLocalVariable(final String name) {
     return new Variable(name, DataType.UNKNOWN);
   }
 
   @Override
-  public void endElement(String uri, String localName, String qName) throws SAXException {
+  public void endElement(
+      final String uri,
+      final String localName,
+      final String qName) throws SAXException {
     assert !nodes.empty();
     try {
       final XMLNodeType currentNode = getNodeType(qName);
@@ -268,9 +276,9 @@ final class XMLConstraintHandler extends DefaultHandler {
       }
 
       nodes.pop();
-    } catch (SAXException e) {
+    } catch (final SAXException e) {
       throw e;
-    } catch (Exception e) {
+    } catch (final Exception e) {
       throw new SAXException(Messages.ERR_INVALID_CONSTRAINT + e.getMessage(), e);
     }
 
@@ -288,7 +296,8 @@ final class XMLConstraintHandler extends DefaultHandler {
   }
 
   @Override
-  public void characters(char ch[], int start, int length) throws SAXException {
+  public void characters(
+      final char ch[], final int start, final int length) throws SAXException {
     assert !nodes.empty();
 
     switch (nodes.lastElement()) {
@@ -312,7 +321,7 @@ final class XMLConstraintHandler extends DefaultHandler {
     // System.out.print(new String(ch, start, length));
   }
 
-  private static XMLNodeType getNodeType(String nodeName) throws SAXException {
+  private static XMLNodeType getNodeType(final String nodeName) throws SAXException {
     final XMLNodeType nodeType = XMLNodeType.fromNodeName(nodeName);
 
     if (null == nodeType) {
@@ -322,8 +331,10 @@ final class XMLConstraintHandler extends DefaultHandler {
     return nodeType;
   }
 
-  private static String getAttribute(String nodeName, Attributes attributes, String attributeName)
-      throws SAXException {
+  private static String getAttribute(
+      final String nodeName,
+      final Attributes attributes,
+      final String attributeName) throws SAXException {
     final String attribute = attributes.getValue(attributeName);
 
     if (null == attribute) {
@@ -334,8 +345,8 @@ final class XMLConstraintHandler extends DefaultHandler {
   }
 
   // XML representation: <Constraint version="1.0">
-  private static void verifyFormatVersion(String nodeName, Attributes attributes)
-      throws SAXException {
+  private static void verifyFormatVersion(
+      final String nodeName, final Attributes attributes) throws SAXException {
     final String versionString = getAttribute(nodeName, attributes, XMLConst.ATTR_FORMAT_VERSION);
 
     if (!versionString.matches("[\\d]+[.][\\d]+")) {
@@ -352,8 +363,9 @@ final class XMLConstraintHandler extends DefaultHandler {
     }
   }
 
-  private static void verifyNodeHierarchy(XMLNodeType current, XMLNodeType parent)
-      throws SAXException {
+  private static void verifyNodeHierarchy(
+      final XMLNodeType current,
+      final XMLNodeType parent) throws SAXException {
     if (!current.isChildOf(parent)) {
       throw new SAXException(String.format(
         Messages.ERR_XML_BAD_HIERARCHY, current.getNodeName(), parent.getNodeName()));
@@ -361,7 +373,9 @@ final class XMLConstraintHandler extends DefaultHandler {
   }
 
   // XML representation: <Value type="(BIT_VECTOR 3)" value="11"/>
-  private static Data getValue(String nodeName, Attributes attributes) throws SAXException {
+  private static Data getValue(
+      final String nodeName,
+      final Attributes attributes) throws SAXException {
     final String typeIdString = getAttribute(nodeName, attributes, XMLConst.ATTR_TYPE_ID);
     final String valueString = getAttribute(nodeName, attributes, XMLConst.ATTR_VALUE);
     final DataType typeInfo = DataType.typeOf(typeIdString);
@@ -370,7 +384,9 @@ final class XMLConstraintHandler extends DefaultHandler {
   }
 
   // XML representation: <Variable name="aaa" type="(BIT_VECTOR 3)" value=""/>
-  private static Variable getVariable(String nodeName, Attributes attributes) throws SAXException {
+  private static Variable getVariable(
+      final String nodeName,
+      final Attributes attributes) throws SAXException {
     final String variableName = getAttribute(nodeName, attributes, XMLConst.ATTR_VARIABLE_NAME);
     final String typeIdString = getAttribute(nodeName, attributes, XMLConst.ATTR_TYPE_ID);
     final String valueString = getAttribute(nodeName, attributes, XMLConst.ATTR_VALUE);
@@ -383,13 +399,16 @@ final class XMLConstraintHandler extends DefaultHandler {
   }
 
   // XML representation: <VariableRef name="aaa"/>
-  private static String getVariableRef(String nodeName, Attributes attributes) throws SAXException {
+  private static String getVariableRef(
+      final String nodeName,
+      final Attributes attributes) throws SAXException {
     return getAttribute(nodeName, attributes, XMLConst.ATTR_VARIABLE_NAME);
   }
 
   // XML representation: <Operation id="BVAND"/>
   @SuppressWarnings({"unchecked", "rawtypes"})
-  private static Enum<?> getOperationId(String nodeName, Attributes attributes) throws Exception {
+  private static Enum<?> getOperationId(
+      final String nodeName, final Attributes attributes) throws Exception {
     final String id = getAttribute(nodeName, attributes, XMLConst.ATTR_OPERATION_ID);
     final String family = getAttribute(nodeName, attributes, XMLConst.ATTR_OPERATION_FAMILY);
 
@@ -402,7 +421,7 @@ final class XMLConstraintHandler extends DefaultHandler {
 /**
  * The XMLConstraintBuilder class implements functionality that build a constraint.
  * 
- * @author Andrei Tatarnikov
+ * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
  */
 
 final class XMLConstraintBuilder {
@@ -412,7 +431,7 @@ final class XMLConstraintBuilder {
   private Formulas formulas = null;
   private Node formula = null;
 
-  private final Stack<OperationBuilder> operations = new Stack<OperationBuilder>();
+  private final Stack<OperationBuilder> operations = new Stack<>();
 
   private void cleanup() {
     constraint = null;
@@ -519,7 +538,7 @@ final class XMLConstraintBuilder {
     }
   }
 
-  public void pushElement(Node se) throws Exception {
+  public void pushElement(final Node se) throws Exception {
     if (operations.empty()) {
       if (null != formula) {
         throw new IllegalStateException(Messages.ERR_FORMULA_ALREADY_ASSIGNED);
@@ -531,7 +550,7 @@ final class XMLConstraintBuilder {
     }
   }
 
-  public void pushOperation(Enum<?> oid) throws Exception {
+  public void pushOperation(final Enum<?> oid) throws Exception {
     if (operations.empty()) {
       throw new IllegalStateException(String.format(
         Messages.ERR_NO_EXPRESSION_FOR_OP, oid.name()));
@@ -540,19 +559,19 @@ final class XMLConstraintBuilder {
     operations.lastElement().setOperationId(oid);
   }
 
-  public void setName(String name) {
+  public void setName(final String name) {
     this.name = name;
   }
 
-  public void setDescription(String description) {
+  public void setDescription(final String description) {
     this.constraint.setDescription(description);
   }
 
-  public void setKind(ConstraintKind kind) {
+  public void setKind(final ConstraintKind kind) {
     this.kind = kind;
   }
 
-  public Variable addGlobalVariable(Variable variable) {
+  public Variable addGlobalVariable(final Variable variable) {
     return constraint.addVariable(variable.getName(), variable.getData());
   }
 
@@ -578,10 +597,10 @@ final class OperationBuilder {
 
   public OperationBuilder() {
     this.operationId = null;
-    this.elements = new ArrayList<Node>();
+    this.elements = new ArrayList<>();
   }
 
-  public void setOperationId(Enum<?> operationId) throws Exception {
+  public void setOperationId(final Enum<?> operationId) throws Exception {
     if (null != this.operationId) {
       throw new Exception(Messages.ERR_EXTRA_OPERATION_ID);
     }
@@ -589,7 +608,7 @@ final class OperationBuilder {
     this.operationId = operationId;
   }
 
-  public void addElement(Node se) throws Exception {
+  public void addElement(final Node se) throws Exception {
     elements.add(se);
   }
 
