f67bb376dc5f613a89b33f9da1f250995c4a038d
Artem Kotsynyak <kotsynyak@ispras.ru>
2017-01-28 13:14:08 +0000

    [transformer] CompositeRule to use multiple rules per operation

---
+++ src/main/java/ru/ispras/fortress/transformer/CompositeRule.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.fortress.transformer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import ru.ispras.fortress.expression.Node;
+
+import static ru.ispras.fortress.util.InvariantChecks.checkNotNull;
+
+/**
+ * Aggregate of rules to be used in {@link NodeTransformer} when multiple
+ * rules per operation required. Rules are checked in sequential order.
+ */
+
+public class CompositeRule implements TransformerRule {
+  private final List<TransformerRule> rules;
+
+  private Node nodeCache = null;
+  private TransformerRule ruleCache = null;
+
+  public CompositeRule(final List<? extends TransformerRule> rules) {
+    checkNotNull(rules);
+    if (rules.isEmpty()) {
+      this.rules = Collections.emptyList();
+    } else {
+      this.rules = new ArrayList<>(rules);
+    }
+  }
+
+  public boolean isApplicable(final Node node) {
+    checkNotNull(node);
+    final TransformerRule rule = findRule(node);
+    if (rule != null) {
+      nodeCache = node;
+      ruleCache = rule;
+      return true;
+    }
+    return false;
+  }
+
+  public Node apply(final Node node) {
+    checkNotNull(node);
+    final TransformerRule rule = (nodeCache == node) ? ruleCache : findRule(node);
+    if (rule != null) {
+      return rule.apply(node);
+    }
+    return node;
+  }
+
+  private TransformerRule findRule(final Node node) {
+    for (final TransformerRule rule : rules) {
+      if (rule.isApplicable(node)) {
+        return rule;
+      }
+    }
+    return null;
+  }
+}
