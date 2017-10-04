fa3e10f968f4cb2b587c0e57219b6e61af545549
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-04 18:34:50 +0300

    Small code update
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/ru/ispras/fortress/data/Variable.java
+++ src/main/java/ru/ispras/fortress/data/Variable.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
- * 
+ * Copyright 2012-2017 ISP RAS (http://www.ispras.ru)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -20,9 +20,9 @@ import ru.ispras.fortress.jaxb.JaxbVariableAdapter;
 import ru.ispras.fortress.util.InvariantChecks;
 
 /**
- * The Variable class describes a variable that can be used as input or output data
- * in constraints.
- * 
+ * The {@link Variable} class describes a variable that can be used as input or
+ * output data in constraints.
+ *
  * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
  */
 @XmlJavaTypeAdapter(JaxbVariableAdapter.class)
@@ -32,10 +32,10 @@ public final class Variable {
 
   /**
    * Constructs a variable from its name and associated data.
-   * 
+   *
    * @param name Variable name.
    * @param data Data the variable refers to.
-   * 
+   *
    * @throws IllegalArgumentException if any of the parameters equals {@code null}.
    */
   public Variable(final String name, final Data data) {
@@ -48,11 +48,11 @@ public final class Variable {
 
   /**
    * Constructs an uninitialized variable of the specified type. The constructed variable does not
-   * refer to any data and its value is set to null.
-   * 
+   * refer to any data and its value is set to {@code null}.
+   *
    * @param name Variable name.
    * @param type Variable type.
-   * 
+   *
    * @throws IllegalArgumentException if any of the parameters equals {@code null}.
    */
   public Variable(final String name, final DataType type) {
@@ -62,7 +62,7 @@ public final class Variable {
   /**
    * Constructs a full copy of the given variable object. The fields are copied by reference because
    * their types are immutable.
-   * 
+   *
    * @param variable Variable object to be copied.
    */
   public Variable(final Variable variable) {
@@ -71,9 +71,9 @@ public final class Variable {
 
   /**
    * Assigns a new data value to the variable.
-   * 
+   *
    * @param data A data value to be assigned to the variable.
-   * 
+   *
    * @throws IllegalArgumentException if the parameter equals {@code null}.
    */
   public void setData(final Data data) {
@@ -83,7 +83,7 @@ public final class Variable {
 
   /**
    * Returns the name of the variable.
-   * 
+   *
    * @return A string representing the variable name.
    */
   public String getName() {
@@ -92,7 +92,7 @@ public final class Variable {
 
   /**
    * Returns a data object associated with the specified variable.
-   * 
+   *
    * @return An Data object associated with the variable.
    */
   public Data getData() {
@@ -101,7 +101,7 @@ public final class Variable {
 
   /**
    * Returns the type of the variable.
-   * 
+   *
    * @return Variable type.
    */
   public DataType getType() {
@@ -110,17 +110,17 @@ public final class Variable {
 
   /**
    * Checks whether the variable has a value assigned to it.
-   * 
+   *
    * @return {@code true} if the variable has a value assigned or {@code false} otherwise.
    */
   public boolean hasValue() {
-    return data.hasValue();
+    return getData().hasValue();
   }
 
   @Override
   public int hashCode() {
     final int prime = 31;
-    return prime * name.hashCode() + data.hashCode();
+    return prime * name.hashCode() + getData().hashCode();
   }
 
   @Override
@@ -142,7 +142,7 @@ public final class Variable {
       return false;
     }
 
-    return data.equals(other.data);
+    return getData().equals(other.getData());
   }
 
   @Override
