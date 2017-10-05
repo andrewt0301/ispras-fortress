b9e7d2cc698cb4c4346562f866fecf5f93a322ec
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-05 16:32:13 +0300

    Unit test update
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/test/java/ru/ispras/fortress/calculator/CalculatorBitVectorTestCase.java
+++ src/test/java/ru/ispras/fortress/calculator/CalculatorBitVectorTestCase.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2015 ISP RAS (http://www.ispras.ru)
- * 
+ * Copyright 2017 ISP RAS (http://www.ispras.ru)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -57,5 +57,13 @@ public final class CalculatorBitVectorTestCase {
         StandardOperation.BVOR, Data.newBitVector(1, 1), Data.newBoolean(false)
         );
 */
+  }
+
+  @Test
+  public void testConcat() {
+    testEquals(
+        Data.newBitVector(0xDEADBEEF, 32),
+        StandardOperation.BVCONCAT, Data.newBitVector(0xBEEF, 16), Data.newBitVector(0xDEAD, 16)
+        );
   }
 }
