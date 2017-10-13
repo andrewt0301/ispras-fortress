11755f0141fb9e964250e122dfc8db9b2e1185a1
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-13 12:10:54 +0300

    Refactoring: Nodes - utility methods
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/test/java/ru/ispras/fortress/solver/constraint/MinCustomOperationTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/constraint/MinCustomOperationTestCase.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2014 ISP RAS (http://www.ispras.ru)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -20,10 +20,9 @@ import java.util.List;
 import ru.ispras.fortress.data.Data;
 import ru.ispras.fortress.data.DataType;
 import ru.ispras.fortress.data.Variable;
-import ru.ispras.fortress.expression.NodeOperation;
 import ru.ispras.fortress.expression.NodeValue;
 import ru.ispras.fortress.expression.NodeVariable;
-import ru.ispras.fortress.expression.StandardOperation;
+import ru.ispras.fortress.expression.Nodes;
 
 public class MinCustomOperationTestCase extends GenericSolverTestBase {
   public MinCustomOperationTestCase() {
@@ -34,23 +33,22 @@ public class MinCustomOperationTestCase extends GenericSolverTestBase {
    * The constraint as described in the SMT language:
    * 
    * <pre>
-   *     (declare-const a Real)
-   *     (declare-const b Real)
-   *     (define-fun MIN ((x Real)(y Real)) Real( ite( >= x y) y x))
-   *     (assert( = a( MIN  3.0  4.0)))
-   *     (assert( = b( MIN a  2.0)))
-   *     (check-sat)
-   *     (get-value ( a b))
-   *     (exit)
+   * (declare-const a Real)
+   * (declare-const b Real)
+   * (define-fun MIN ((x Real)(y Real)) Real( ite( >= x y) y x))
+   * (assert( = a( MIN  3.0  4.0)))
+   * (assert( = b( MIN a  2.0)))
+   * (check-sat)
+   * (get-value ( a b))
+   * (exit)
    * </pre>
-   * 
+   *
    * Expected output:
-   * 
+   *
    * <pre>
-   *     sat ((a 3.0)(b 2.0))
+   * sat ((a 3.0)(b 2.0))
    * </pre>
    */
-
   public static class MinCustomOperation implements SampleConstraint {
     @Override
     public Constraint getConstraint() {
@@ -68,17 +66,10 @@ public class MinCustomOperationTestCase extends GenericSolverTestBase {
       final Formulas formulas = new Formulas();
       builder.setInnerRep(formulas);
 
-      formulas.add(new NodeOperation(StandardOperation.EQ, a, new NodeOperation(
-          StandardOperation.MIN, NodeValue.newReal(3.0), NodeValue.newReal(4.0))));
-
-      formulas.add(new NodeOperation(StandardOperation.EQ, b, new NodeOperation(
-          StandardOperation.MIN, a, NodeValue.newReal(2.0))));
-
-      formulas.add(new NodeOperation(StandardOperation.EQ, c, new NodeOperation(
-          StandardOperation.MIN, NodeValue.newInteger(3), NodeValue.newInteger(4))));
-
-      formulas.add(new NodeOperation(StandardOperation.EQ, d, new NodeOperation(
-          StandardOperation.MIN, c, NodeValue.newInteger(2))));
+      formulas.add(Nodes.EQ(a, Nodes.MIN(NodeValue.newReal(3.0), NodeValue.newReal(4.0))));
+      formulas.add(Nodes.EQ(b, Nodes.MIN(a, NodeValue.newReal(2.0))));
+      formulas.add(Nodes.EQ(c, Nodes.MIN(NodeValue.newInteger(3), NodeValue.newInteger(4))));
+      formulas.add(Nodes.EQ(d, Nodes.MIN(c, NodeValue.newInteger(2))));
 
       return builder.build();
     }
