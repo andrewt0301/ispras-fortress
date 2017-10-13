e6cc11fdf4c37c28e7e800d118443176a009e807
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-13 14:09:47 +0300

    Refactoring: Nodes - utility methods
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/test/java/ru/ispras/fortress/solver/constraint/BooleanToBitVectorCastTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/constraint/BooleanToBitVectorCastTestCase.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014 ISP RAS (http://www.ispras.ru), UniTESK Lab (http://www.unitesk.com)
+ * Copyright 2014-2017 ISP RAS (http://www.ispras.ru)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,16 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package ru.ispras.fortress.solver.constraint;
 
 import ru.ispras.fortress.data.Data;
 import ru.ispras.fortress.data.DataType;
 import ru.ispras.fortress.data.Variable;
 import ru.ispras.fortress.data.types.bitvector.BitVector;
-import ru.ispras.fortress.expression.NodeOperation;
 import ru.ispras.fortress.expression.NodeValue;
 import ru.ispras.fortress.expression.NodeVariable;
-import ru.ispras.fortress.expression.StandardOperation;
+import ru.ispras.fortress.expression.Nodes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,10 +65,8 @@ public class BooleanToBitVectorCastTestCase extends GenericSolverTestBase {
    * (get-value ( x y z))
    * (get-model)
    * (exit)
-   * 
    */
   public static class BooleanToBitVectorCast implements SampleConstraint {
-
     @Override
     public Constraint getConstraint() {
       final ConstraintBuilder builder = new ConstraintBuilder();
@@ -84,18 +82,12 @@ public class BooleanToBitVectorCastTestCase extends GenericSolverTestBase {
       final Formulas formulas = new Formulas();
       builder.setInnerRep(formulas);
 
-      formulas.add(new NodeOperation(StandardOperation.LESS, x, NodeValue.newInteger(2)));
+      formulas.add(Nodes.LESS(x, NodeValue.newInteger(2)));
+      formulas.add(Nodes.GREATER(y, NodeValue.newInteger(0)));
 
-      formulas.add(new NodeOperation(StandardOperation.GREATER, y, NodeValue.newInteger(0)));
-
-      formulas.add(
-          new NodeOperation(
-              StandardOperation.EQ,
-              new NodeOperation(
-                  StandardOperation.BVOR,
-                  new NodeOperation(StandardOperation.BOOL2BV, new NodeOperation(StandardOperation.EQ, x, y)), z),
-              NodeValue.newBitVector(BitVector.valueOf(true)))
-              );
+      formulas.add(Nodes.EQ(
+          Nodes.BVOR(Nodes.BOOL2BV(Nodes.EQ(x, y)), z),
+          NodeValue.newBitVector(BitVector.valueOf(true))));
 
       return builder.build();
     }
