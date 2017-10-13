2866949a6cffd8d7ab639b53f9d44568e9bd7e9e
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-13 12:07:10 +0300

    Refactoring: Nodes - utility methods
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/test/java/ru/ispras/fortress/solver/constraint/ConstraintNegationTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/constraint/ConstraintNegationTestCase.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2014 ISP RAS (http://www.ispras.ru)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -19,8 +19,7 @@ import java.util.List;
 
 import ru.ispras.fortress.data.Variable;
 import ru.ispras.fortress.expression.Node;
-import ru.ispras.fortress.expression.NodeOperation;
-import ru.ispras.fortress.expression.StandardOperation;
+import ru.ispras.fortress.expression.Nodes;
 
 public class ConstraintNegationTestCase extends GenericSolverTestBase {
   public ConstraintNegationTestCase() {
@@ -40,22 +39,21 @@ public class ConstraintNegationTestCase extends GenericSolverTestBase {
       builder.setName("ConstraintNegation");
       builder.setKind(constraint.getKind());
 
-      for (Variable var : constraint.getVariables())
+      for (final Variable var : constraint.getVariables()) {
         builder.addVariable(var.getName(), var.getData());
+      }
 
       final Formulas formulas = new Formulas();
       builder.setInnerRep(formulas);
 
       // not(= (a not(not(a))))
-      formulas.add(new NodeOperation(StandardOperation.NOT, new NodeOperation(StandardOperation.EQ,
-          formula, new NodeOperation(StandardOperation.NOT, negFormula))));
+      formulas.add(Nodes.NOT(Nodes.EQ(formula, Nodes.NOT(negFormula))));
 
       // (not(a) and a)
-      formulas.add(new NodeOperation(StandardOperation.AND, negFormula, formula));
+      formulas.add(Nodes.AND(negFormula, formula));
 
       // not((not(a)) or a)
-      formulas.add(new NodeOperation(
-        StandardOperation.NOT, new NodeOperation(StandardOperation.OR, negFormula, formula)));
+      formulas.add(Nodes.NOT(Nodes.OR(negFormula, formula)));
 
       return ConstraintCombiner.makeNegation(builder.build());
     }
