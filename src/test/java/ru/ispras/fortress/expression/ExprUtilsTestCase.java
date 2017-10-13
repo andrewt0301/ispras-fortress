c03d935fd313b8e44e523cbe80dd82a16374009e
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-13 12:52:15 +0300

    Refactoring: Nodes - utility methods
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/test/java/ru/ispras/fortress/expression/ExprUtilsTestCase.java
+++ src/test/java/ru/ispras/fortress/expression/ExprUtilsTestCase.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2014 ISP RAS (http://www.ispras.ru)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -30,29 +30,16 @@ import ru.ispras.fortress.expression.StandardOperation;
 
 public final class ExprUtilsTestCase {
   private static final NodeVariable x = new NodeVariable(new Variable("x", DataType.INTEGER));
-
   private static final NodeVariable y = new NodeVariable(new Variable("y", DataType.INTEGER));
-
   private static final NodeVariable z = new NodeVariable(new Variable("z", DataType.INTEGER));
-
   private static final NodeVariable i = new NodeVariable(new Variable("i", DataType.INTEGER));
-
   private static final NodeVariable j = new NodeVariable(new Variable("j", DataType.INTEGER));
 
-  private static final Node xEq0 = new NodeOperation(StandardOperation.EQ, x,
-      NodeValue.newInteger(0));
-
-  private static final Node yEq5 = new NodeOperation(StandardOperation.EQ, y,
-      NodeValue.newInteger(5));
-
-  private static final Node zEq10 = new NodeOperation(StandardOperation.EQ, z,
-      NodeValue.newInteger(10));
-
-  private static final Node iEq15 = new NodeOperation(StandardOperation.EQ, i,
-      NodeValue.newInteger(15));
-
-  private static final Node jEq20 = new NodeOperation(StandardOperation.EQ, j,
-      NodeValue.newInteger(20));
+  private static final Node xEq0 = Nodes.EQ(x, NodeValue.newInteger(0));
+  private static final Node yEq5 = Nodes.EQ( y, NodeValue.newInteger(5));
+  private static final Node zEq10 = Nodes.EQ( z, NodeValue.newInteger(10));
+  private static final Node iEq15 = Nodes.EQ( i, NodeValue.newInteger(15));
+  private static final Node jEq20 = Nodes.EQ( j, NodeValue.newInteger(20));
 
   @Test
   public void testIsCondition() {
@@ -63,14 +50,10 @@ public final class ExprUtilsTestCase {
 
     assertTrue(ExprUtils.isCondition(new NodeVariable(new Variable("x", DataType.BOOLEAN))));
     assertFalse(ExprUtils.isCondition(new NodeVariable(new Variable("y", DataType.INTEGER))));
+    assertTrue(ExprUtils.isCondition(Nodes.EQ(NodeValue.newInteger(1), NodeValue.newInteger(2))));
+    assertFalse(ExprUtils.isCondition(Nodes.ADD(NodeValue.newInteger(1), NodeValue.newInteger(2))));
 
-    assertTrue(ExprUtils.isCondition(new NodeOperation(StandardOperation.EQ, NodeValue
-        .newInteger(1), NodeValue.newInteger(2))));
-
-    assertFalse(ExprUtils.isCondition(new NodeOperation(StandardOperation.ADD, NodeValue
-        .newInteger(1), NodeValue.newInteger(2))));
-
-    assertTrue(ExprUtils.isCondition(new NodeOperation(StandardOperation.OR, new NodeOperation(
+    assertTrue(ExprUtils.isCondition(Nodes.OR(new NodeOperation(
         StandardOperation.GREATEREQ, x, NodeValue.newInteger(0)), new NodeOperation(
         StandardOperation.LESS, x, NodeValue.newInteger(10)))));
   }
@@ -80,23 +63,22 @@ public final class ExprUtilsTestCase {
     assertTrue(ExprUtils.isAtomicCondition(NodeValue.newBoolean(true)));
     assertTrue(ExprUtils.isAtomicCondition(NodeValue.newBoolean(false)));
 
-    assertTrue(ExprUtils.isAtomicCondition(new NodeOperation(StandardOperation.EQ, NodeValue
+    assertTrue(ExprUtils.isAtomicCondition(Nodes.EQ( NodeValue
         .newInteger(1), NodeValue.newInteger(2))));
 
-    assertFalse(ExprUtils.isAtomicCondition(new NodeOperation(StandardOperation.ADD, NodeValue
+    assertFalse(ExprUtils.isAtomicCondition(Nodes.ADD(NodeValue
         .newInteger(1), NodeValue.newInteger(2))));
 
     assertFalse(ExprUtils.isAtomicCondition(new NodeOperation(StandardOperation.OR,
-        new NodeOperation(StandardOperation.GREATEREQ, x, NodeValue.newInteger(0)),
-        new NodeOperation(StandardOperation.LESS, x, NodeValue.newInteger(10)))));
+        Nodes.GREATEREQ(x, NodeValue.newInteger(0)),
+        Nodes.LESS(x, NodeValue.newInteger(10)))));
   }
 
   @Test
   public void testGetConjunction() {
-    final Node iEq15ORjEq20 = new NodeOperation(StandardOperation.OR, iEq15, jEq20);
+    final Node iEq15ORjEq20 = Nodes.OR(iEq15, jEq20);
 
-    final Node expected = new NodeOperation(StandardOperation.AND, xEq0, yEq5, zEq10, iEq15ORjEq20);
-
+    final Node expected = Nodes.AND(xEq0, yEq5, zEq10, iEq15ORjEq20);
     final Node actual = ExprUtils.getConjunction(xEq0, yEq5, zEq10, iEq15ORjEq20);
 
     assertEquals(expected, actual);
@@ -104,10 +86,9 @@ public final class ExprUtilsTestCase {
 
   @Test
   public void testGetDisjunction() {
-    final Node iEq15ANDjEq20 = new NodeOperation(StandardOperation.AND, iEq15, jEq20);
+    final Node iEq15ANDjEq20 = Nodes.AND(iEq15, jEq20);
 
-    final Node expected = new NodeOperation(StandardOperation.OR, xEq0, yEq5, zEq10, iEq15ANDjEq20);
-
+    final Node expected = Nodes.OR(xEq0, yEq5, zEq10, iEq15ANDjEq20);
     final Node actual = ExprUtils.getDisjunction(xEq0, yEq5, zEq10, iEq15ANDjEq20);
 
     assertEquals(expected, actual);
@@ -115,12 +96,9 @@ public final class ExprUtilsTestCase {
 
   @Test
   public void testGetNegation() {
-    final Node iEq15ORjEq20 = new NodeOperation(StandardOperation.OR, iEq15, jEq20);
+    final Node iEq15ORjEq20 = Nodes.OR(iEq15, jEq20);
 
-    final Node expected =
-        new NodeOperation(StandardOperation.NOT, new NodeOperation(StandardOperation.AND, xEq0,
-            yEq5, zEq10, iEq15ORjEq20));
-
+    final Node expected = Nodes.NOT(Nodes.AND(xEq0, yEq5, zEq10, iEq15ORjEq20));
     final Node actual = ExprUtils.getNegation(xEq0, yEq5, zEq10, iEq15ORjEq20);
 
     assertEquals(expected, actual);
@@ -128,12 +106,9 @@ public final class ExprUtilsTestCase {
 
   @Test
   public void testGetComplement() {
-    final Node iEq15ANDjEq20 = new NodeOperation(StandardOperation.AND, iEq15, jEq20);
+    final Node iEq15ANDjEq20 = Nodes.AND(iEq15, jEq20);
 
-    final Node expected =
-        new NodeOperation(StandardOperation.NOT, new NodeOperation(StandardOperation.OR, xEq0,
-            yEq5, zEq10, iEq15ANDjEq20));
-
+    final Node expected = Nodes.NOT(Nodes.OR(xEq0, yEq5, zEq10, iEq15ANDjEq20));
     final Node actual = ExprUtils.getComplement(xEq0, yEq5, zEq10, iEq15ANDjEq20);
 
     assertEquals(expected, actual);
@@ -142,30 +117,29 @@ public final class ExprUtilsTestCase {
   @Test
   public void testAreComplete() {
     assertTrue(ExprUtils.areComplete(
-        new NodeOperation(StandardOperation.GREATEREQ, x, NodeValue.newInteger(0)),
-        new NodeOperation(StandardOperation.LESS, x, NodeValue.newInteger(10))));
+        Nodes.GREATEREQ(x, NodeValue.newInteger(0)),
+        Nodes.LESS(x, NodeValue.newInteger(10))));
 
     assertFalse(ExprUtils.areComplete(
-        new NodeOperation(StandardOperation.LESS, x, NodeValue.newInteger(0)), new NodeOperation(
-            StandardOperation.GREATEREQ, x, NodeValue.newInteger(10))));
+        Nodes.LESS(x, NodeValue.newInteger(0)),
+        Nodes.GREATEREQ(x, NodeValue.newInteger(10))));
   }
 
   @Test
   public void testAreCompatible() {
     assertTrue(ExprUtils.areCompatible(
-        new NodeOperation(StandardOperation.GREATEREQ, x, NodeValue.newInteger(0)),
-        new NodeOperation(StandardOperation.LESS, x, NodeValue.newInteger(10))));
+        Nodes.GREATEREQ(x, NodeValue.newInteger(0)),
+        Nodes.LESS(x, NodeValue.newInteger(10))));
 
     assertFalse(ExprUtils.areCompatible(
-        new NodeOperation(StandardOperation.LESS, x, NodeValue.newInteger(0)), new NodeOperation(
-            StandardOperation.GREATEREQ, x, NodeValue.newInteger(10))));
+        Nodes.LESS(x, NodeValue.newInteger(0)),
+        Nodes.GREATEREQ(x, NodeValue.newInteger(10))));
   }
 
   @Test
   public void testHasBindings() {
     final Node noBindings =
-        new NodeOperation(StandardOperation.AND, xEq0, yEq5, zEq10, new NodeOperation(
-            StandardOperation.OR, iEq15, jEq20));
+        Nodes.AND(xEq0, yEq5, zEq10, new NodeOperation(StandardOperation.OR, iEq15, jEq20));
 
     assertFalse(ExprUtils.hasBindings(noBindings));
 
@@ -173,13 +147,13 @@ public final class ExprUtilsTestCase {
     final NodeVariable b = new NodeVariable(new Variable("b", DataType.INTEGER));
 
     final Node bindings =
-        new NodeOperation(StandardOperation.AND, xEq0, yEq5, zEq10, new NodeBinding(
-            new NodeOperation(StandardOperation.OR, iEq15, jEq20), NodeBinding.bindVariable(a,
+        Nodes.AND(xEq0, yEq5, zEq10, new NodeBinding(
+            Nodes.OR(iEq15, jEq20), NodeBinding.bindVariable(a,
                 NodeValue.newInteger(3)), NodeBinding.bindVariable(b, NodeValue.newInteger(4)),
             NodeBinding.bindVariable(i,
-                new NodeOperation(StandardOperation.MUL, a, NodeValue.newInteger(5))),
+                Nodes.MUL(a, NodeValue.newInteger(5))),
             NodeBinding.bindVariable(j,
-                new NodeOperation(StandardOperation.MUL, b, NodeValue.newInteger(5)))));
+                Nodes.MUL(b, NodeValue.newInteger(5)))));
 
     assertTrue(ExprUtils.hasBindings(bindings));
   }
@@ -187,20 +161,16 @@ public final class ExprUtilsTestCase {
   @Test
   public void testIsConstant() {
     final Node expr1 =
-        new NodeOperation(StandardOperation.PLUS, NodeValue.newInteger(1), new NodeOperation(
-            StandardOperation.MUL, NodeValue.newInteger(2), NodeValue.newInteger(3)),
-            new NodeOperation(StandardOperation.SUB, NodeValue.newInteger(20), new NodeOperation(
-                StandardOperation.MUL, NodeValue.newInteger(2), NodeValue.newInteger(5))));
+        Nodes.ADD(NodeValue.newInteger(1), Nodes.MUL(NodeValue.newInteger(2), NodeValue.newInteger(3)),
+            Nodes.SUB(NodeValue.newInteger(20), Nodes.MUL(NodeValue.newInteger(2), NodeValue.newInteger(5))));
 
     // Constant (no variables, no bindings).
     assertTrue(ExprUtils.isConstant(expr1));
 
     final NodeVariable x = new NodeVariable(new Variable("x", DataType.INTEGER));
     final Node expr2 =
-        new NodeOperation(StandardOperation.PLUS, NodeValue.newInteger(1), new NodeOperation(
-            StandardOperation.MUL, NodeValue.newInteger(2), NodeValue.newInteger(3)),
-            new NodeOperation(StandardOperation.SUB, NodeValue.newInteger(20), new NodeOperation(
-                StandardOperation.MUL, NodeValue.newInteger(2), x)));
+        Nodes.ADD(NodeValue.newInteger(1), Nodes.MUL(NodeValue.newInteger(2), NodeValue.newInteger(3)),
+            Nodes.SUB(NodeValue.newInteger(20), Nodes.MUL(NodeValue.newInteger(2), x)));
 
     // Non-constant: has a variable
     assertFalse(ExprUtils.isConstant(expr2));
@@ -212,22 +182,18 @@ public final class ExprUtilsTestCase {
 
     final NodeVariable y = new NodeVariable(new Variable("y", DataType.INTEGER));
     final Node expr3 =
-        new NodeOperation(StandardOperation.PLUS, NodeValue.newInteger(1), new NodeOperation(
-            StandardOperation.MUL, NodeValue.newInteger(2), NodeValue.newInteger(3)),
+        Nodes.ADD(NodeValue.newInteger(1), Nodes.MUL(NodeValue.newInteger(2), NodeValue.newInteger(3)),
 
-        new NodeBinding(new NodeOperation(StandardOperation.SUB, NodeValue.newInteger(20),
-            new NodeOperation(StandardOperation.MUL, NodeValue.newInteger(2), y)),
-            NodeBinding.bindVariable(y, NodeValue.newInteger(10))));
+        new NodeBinding(Nodes.SUB(NodeValue.newInteger(20), Nodes.MUL(NodeValue.newInteger(2), y)),  NodeBinding.bindVariable(y, NodeValue.newInteger(10))));
 
     // Constant: has a variable, but it is bound to a constant value
     assertTrue(ExprUtils.isConstant(expr3));
 
     final Node expr4 =
-        new NodeOperation(StandardOperation.PLUS, NodeValue.newInteger(1), new NodeOperation(
-            StandardOperation.MUL, NodeValue.newInteger(2), y),
+        Nodes.ADD(NodeValue.newInteger(1), Nodes.MUL(NodeValue.newInteger(2), y),
 
-        new NodeBinding(new NodeOperation(StandardOperation.SUB, NodeValue.newInteger(20),
-            new NodeOperation(StandardOperation.MUL, NodeValue.newInteger(2), y)),
+        new NodeBinding(Nodes.SUB(NodeValue.newInteger(20), 
+            Nodes.MUL(NodeValue.newInteger(2), y)),
             NodeBinding.bindVariable(y, NodeValue.newInteger(10))));
 
     // Non-constant: has a variable, but it is bound to a constant value
@@ -240,13 +206,14 @@ public final class ExprUtilsTestCase {
     assertTrue(ExprUtils.isSAT(NodeValue.newBoolean(true)));
     assertFalse(ExprUtils.isSAT(NodeValue.newBoolean(false)));
 
-    assertTrue(ExprUtils
-        .isSAT(new NodeOperation(StandardOperation.EQ, NodeValue.newInteger(5), new NodeOperation(
-            StandardOperation.ADD, NodeValue.newInteger(2), NodeValue.newInteger(3)))));
+    assertTrue(ExprUtils.isSAT(
+        Nodes.EQ(NodeValue.newInteger(5),
+                 Nodes.ADD(NodeValue.newInteger(2), NodeValue.newInteger(3)))));
 
-    assertFalse(ExprUtils
-        .isSAT(new NodeOperation(StandardOperation.EQ, NodeValue.newInteger(5), new NodeOperation(
-            StandardOperation.ADD, NodeValue.newInteger(2), NodeValue.newInteger(-3)))));
+    assertFalse(ExprUtils.isSAT(
+        Nodes.EQ(
+            NodeValue.newInteger(5),
+            Nodes.ADD(NodeValue.newInteger(2), NodeValue.newInteger(-3)))));
   }
 
   @Test
@@ -261,13 +228,13 @@ public final class ExprUtilsTestCase {
         Node.Kind.VALUE,
         NodeValue.newInteger(10),
         NodeValue.newReal(3.14),
-        new NodeOperation(StandardOperation.ADD, NodeValue.newInteger(10), NodeValue.newInteger(20)),
+        Nodes.ADD(NodeValue.newInteger(10), NodeValue.newInteger(20)),
         NodeValue.newBitVector(BitVector.valueOf(0xDEADBEEF, 32))));
 
     assertTrue(ExprUtils.isKind(
         Node.Kind.OPERATION,
-        new NodeOperation(StandardOperation.ADD, NodeValue.newInteger(10), NodeValue.newInteger(20)),
-        new NodeOperation(StandardOperation.SUB, NodeValue.newInteger(10), NodeValue.newInteger(20)),
-        new NodeOperation(StandardOperation.MOD, NodeValue.newInteger(10), NodeValue.newInteger(20))));
+        Nodes.ADD(NodeValue.newInteger(10), NodeValue.newInteger(20)),
+        Nodes.SUB(NodeValue.newInteger(10), NodeValue.newInteger(20)),
+        Nodes.MOD(NodeValue.newInteger(10), NodeValue.newInteger(20))));
   }
 }
