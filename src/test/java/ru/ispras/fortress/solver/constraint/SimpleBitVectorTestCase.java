592889e62757a86128d6d6e8f513ba03c6c48d97
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-13 14:15:06 +0300

    Refactoring: Nodes - utility methods
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/test/java/ru/ispras/fortress/solver/constraint/SimpleBitVectorTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/constraint/SimpleBitVectorTestCase.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
- * 
+ * Copyright 2012-2017 ISP RAS (http://www.ispras.ru)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -19,10 +19,9 @@ import java.util.List;
 
 import ru.ispras.fortress.data.DataType;
 import ru.ispras.fortress.data.Variable;
-import ru.ispras.fortress.expression.NodeOperation;
 import ru.ispras.fortress.expression.NodeValue;
 import ru.ispras.fortress.expression.NodeVariable;
-import ru.ispras.fortress.expression.StandardOperation;
+import ru.ispras.fortress.expression.Nodes;
 
 public final class SimpleBitVectorTestCase extends GenericSolverTestBase {
   public SimpleBitVectorTestCase() {
@@ -31,22 +30,21 @@ public final class SimpleBitVectorTestCase extends GenericSolverTestBase {
 
   /**
    * The constraint as described in the SMT language:
-   * 
+   *
    * <pre>
-   *     (declare-const a (_ BitVec 3))
-   *     (declare-const b (_ BitVec 3))
-   *     (assert (not (= a b)))
-   *     (assert (= (bvor a b) #b111))
-   *     (assert (= (bvand a b) #b000))
-   *     (assert (= (bvshl a (_ bv3 3))(bvsmod a (_ bv2 3))))
-   *     (check-sat)
-   *     (get-value (a b))
-   *     (exit)
+   * (declare-const a (_ BitVec 3))
+   * (declare-const b (_ BitVec 3))
+   * (assert (not (= a b)))
+   * (assert (= (bvor a b) #b111))
+   * (assert (= (bvand a b) #b000))
+   * (assert (= (bvshl a (_ bv3 3))(bvsmod a (_ bv2 3))))
+   * (check-sat)
+   * (get-value (a b))
+   * (exit)
    * </pre>
-   * 
+   *
    * Expected output: sat ((a #b010) (b #b101))
    */
-
   public static class SimpleBitVector implements SampleConstraint {
     private static final int BIT_VECTOR_SIZE = 3;
     private static final DataType BIT_VECTOR_TYPE = DataType.BIT_VECTOR(BIT_VECTOR_SIZE);
@@ -65,19 +63,14 @@ public final class SimpleBitVectorTestCase extends GenericSolverTestBase {
       final Formulas formulas = new Formulas();
       builder.setInnerRep(formulas);
 
-      formulas.add(new NodeOperation(StandardOperation.NOT, new NodeOperation(StandardOperation.EQ,
-          a, b)));
+      formulas.add(Nodes.NOT(Nodes.EQ(a, b)));
+      formulas.add(Nodes.EQ(Nodes.BVOR(a, b), new NodeValue(BIT_VECTOR_TYPE.valueOf("111", 2))));
 
-      formulas.add(new NodeOperation(StandardOperation.EQ, new NodeOperation(
-          StandardOperation.BVOR, a, b), new NodeValue(BIT_VECTOR_TYPE.valueOf("111", 2))));
+      formulas.add(Nodes.EQ(
+          Nodes.BVLSHL(a, new NodeValue(BIT_VECTOR_TYPE.valueOf("3", 10))),
+          Nodes.BVSMOD(a, new NodeValue(BIT_VECTOR_TYPE.valueOf("2", 10)))));
 
-      formulas.add(new NodeOperation(StandardOperation.EQ, new NodeOperation(
-          StandardOperation.BVLSHL, a, new NodeValue(BIT_VECTOR_TYPE.valueOf("3", 10))),
-          new NodeOperation(StandardOperation.BVSMOD, a, new NodeValue(BIT_VECTOR_TYPE.valueOf("2",
-              10)))));
-
-      formulas.add(new NodeOperation(StandardOperation.EQ, new NodeOperation(
-          StandardOperation.BVAND, a, b), new NodeValue(BIT_VECTOR_TYPE.valueOf("0", 2))));
+      formulas.add(Nodes.EQ(Nodes.BVAND(a, b), new NodeValue(BIT_VECTOR_TYPE.valueOf("0", 2))));
 
       return builder.build();
     }
