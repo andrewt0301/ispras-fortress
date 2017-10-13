7acdfa780fed7e9bc8ca1f588a5f307de4c1d9e1
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-13 15:24:21 +0300

    Refactoring
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/test/java/ru/ispras/fortress/solver/constraint/RepeatOperationTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/constraint/RepeatOperationTestCase.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014 ISP RAS (http://www.ispras.ru)
- * 
+ * Copyright 2014-2017 ISP RAS (http://www.ispras.ru)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -22,6 +22,7 @@ import ru.ispras.fortress.data.Variable;
 import ru.ispras.fortress.expression.NodeOperation;
 import ru.ispras.fortress.expression.NodeValue;
 import ru.ispras.fortress.expression.NodeVariable;
+import ru.ispras.fortress.expression.Nodes;
 import ru.ispras.fortress.expression.StandardOperation;
 import ru.ispras.fortress.transformer.ReduceOptions;
 import ru.ispras.fortress.transformer.Reducer;
@@ -33,20 +34,19 @@ public final class RepeatOperationTestCase extends GenericSolverTestBase {
 
   /**
    * The constraint as described in the SMT language:
-   * 
+   *
    * <pre>
-   *     (declare-const x (_ BitVec 4))
-   *     (declare-const y (_ BitVec 16))
-   *     (assert (= x #x5))
-   *     (assert (= y ((_ repeat 4) x)))
-   *     (check-sat)
-   *     (get-value (x y))
-   *     (exit)
+   * (declare-const x (_ BitVec 4))
+   * (declare-const y (_ BitVec 16))
+   * (assert (= x #x5))
+   * (assert (= y ((_ repeat 4) x)))
+   * (check-sat)
+   * (get-value (x y))
+   * (exit)
    * </pre>
-   * 
+   *
    * Expected output: sat ((x #x5)(y #x5555))
    */
-
   public static class RepeatOperation implements SampleConstraint {
     private static final int BIT_VECTOR_ARG_SIZE = 4;
     private static final DataType BIT_VECTOR_ARG_TYPE = DataType.BIT_VECTOR(BIT_VECTOR_ARG_SIZE);
@@ -71,14 +71,15 @@ public final class RepeatOperationTestCase extends GenericSolverTestBase {
       final Formulas formulas = new Formulas();
       builder.setInnerRep(formulas);
 
-      formulas.add(new NodeOperation(StandardOperation.EQ, x, new NodeValue(BIT_VECTOR_ARG_TYPE
-          .valueOf("5", HEX_RADIX))));
-
-      formulas.add(new NodeOperation(StandardOperation.EQ, y, new NodeOperation(
-          StandardOperation.BVREPEAT, Reducer.reduce(
-              ReduceOptions.NEW_INSTANCE,
-              new NodeOperation(StandardOperation.ADD, new NodeValue(INT_TYPE.valueOf("2",
-                  DEC_RADIX)), new NodeValue(INT_TYPE.valueOf("2", DEC_RADIX)))), x)));
+      formulas.add(Nodes.EQ(x, new NodeValue(BIT_VECTOR_ARG_TYPE.valueOf("5", HEX_RADIX))));
+      formulas.add(Nodes.EQ(
+          y,
+          Nodes.BVREPEAT(
+              (NodeValue) Reducer.reduce(
+                  ReduceOptions.NEW_INSTANCE,
+                  Nodes.ADD(new NodeValue(INT_TYPE.valueOf("2", DEC_RADIX)),
+                            new NodeValue(INT_TYPE.valueOf("2", DEC_RADIX)))),
+              x)));
 
       return builder.build();
     }
