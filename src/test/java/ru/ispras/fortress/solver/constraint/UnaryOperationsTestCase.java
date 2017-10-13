e1340231ed6a9c8a8f82dd649336ec4975678686
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-13 14:17:57 +0300

    Refactoring: Nodes - utility methods
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/test/java/ru/ispras/fortress/solver/constraint/UnaryOperationsTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/constraint/UnaryOperationsTestCase.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014 ISP RAS (http://www.ispras.ru)
- * 
+ * Copyright 2014-217 ISP RAS (http://www.ispras.ru)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -19,10 +19,9 @@ import java.util.List;
 
 import ru.ispras.fortress.data.DataType;
 import ru.ispras.fortress.data.Variable;
-import ru.ispras.fortress.expression.NodeOperation;
 import ru.ispras.fortress.expression.NodeValue;
 import ru.ispras.fortress.expression.NodeVariable;
-import ru.ispras.fortress.expression.StandardOperation;
+import ru.ispras.fortress.expression.Nodes;
 
 public class UnaryOperationsTestCase extends GenericSolverTestBase {
   public UnaryOperationsTestCase() {
@@ -31,20 +30,19 @@ public class UnaryOperationsTestCase extends GenericSolverTestBase {
 
   /**
    * The constraint as described in the SMT-LIB language:
-   * 
+   *
    * <pre>
-   *     (declare-const a Int)
-   *     (declare-const b Int)
-   *     (assert (= (~ a) 5))
-   *     (assert (= (+ b) 1))
-   *     (check-sat)
-   *     (get-value (a b))
-   *     (exit)
+   * (declare-const a Int)
+   * (declare-const b Int)
+   * (assert (= (~ a) 5))
+   * (assert (= (+ b) 1))
+   * (check-sat)
+   * (get-value (a b))
+   * (exit)
    * </pre>
-   * 
+   *
    * Expected output: sat ((x (- 5)) (y 1))
    */
-
   public static class UnaryOperations implements SampleConstraint {
     private static final DataType intType = DataType.INTEGER;
 
@@ -62,11 +60,8 @@ public class UnaryOperationsTestCase extends GenericSolverTestBase {
       final Formulas formulas = new Formulas();
       builder.setInnerRep(formulas);
 
-      formulas.add(new NodeOperation(StandardOperation.EQ, new NodeOperation(
-          StandardOperation.MINUS, a), new NodeValue(intType.valueOf("5", 10))));
-
-      formulas.add(new NodeOperation(StandardOperation.EQ, new NodeOperation(
-          StandardOperation.PLUS, b), new NodeValue(intType.valueOf("1", 10))));
+      formulas.add(Nodes.EQ(Nodes.MINUS(a), new NodeValue(intType.valueOf("5", 10))));
+      formulas.add(Nodes.EQ(Nodes.PLUS(b), new NodeValue(intType.valueOf("1", 10))));
 
       return builder.build();
     }
