052f697b814ab86c946920919e254454e2d4df99
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-17 11:35:42 +0300

    Small update
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/ru/ispras/fortress/expression/ExprTreeVisitorDefault.java
+++ src/main/java/ru/ispras/fortress/expression/ExprTreeVisitorDefault.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2014-2016 ISP RAS (http://www.ispras.ru)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -25,10 +25,10 @@ import ru.ispras.fortress.util.InvariantChecks;
  * class helps keep the size of other implementations of the {@code ExprTreeVisitor} interface
  * to minimum when it is required to implement only a small number of {@code ExprTreeVisitor}
  * methods to perform some actions on the expression tree being visited.
- * 
+ *
  * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
  */
-public abstract class ExprTreeVisitorDefault implements ExprTreeVisitor {
+public class ExprTreeVisitorDefault implements ExprTreeVisitor {
   private Status status;
 
   /**
