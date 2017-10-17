ba700f76922658e6bb3eb53ca39e0aaf438bb02c
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-17 09:57:05 +0300

    Refactoring
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/test/java/ru/ispras/fortress/solver/constraint/BVUGTOperation.java
+++ src/test/java/ru/ispras/fortress/solver/constraint/BVUGTOperation.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014 ISP RAS (http://www.ispras.ru)
- * 
+ * Copyright 2014-2017 ISP RAS (http://www.ispras.ru)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -19,12 +19,14 @@ import java.util.List;
 
 import ru.ispras.fortress.data.DataType;
 import ru.ispras.fortress.data.Variable;
-import ru.ispras.fortress.expression.*;
+import ru.ispras.fortress.expression.NodeValue;
+import ru.ispras.fortress.expression.NodeVariable;
+import ru.ispras.fortress.expression.Nodes;
 import ru.ispras.fortress.solver.constraint.GenericSolverTestBase.SampleConstraint;
 
 /**
  * The constraint as described in the SMT-LIB language:
- * 
+ *
  * <pre>
  * (declare-const x (_ BitVec 32))
  * (assert (bvugt x (_ bv100 32)))
@@ -32,14 +34,13 @@ import ru.ispras.fortress.solver.constraint.GenericSolverTestBase.SampleConstrai
  * (get-value (x))
  * (exit)
  * </pre>
- * 
+ *
  * Expected output:
- * 
+ *
  * <pre>
  * sat ((x #x00000070))
  * </pre>
  */
-
 public class BVUGTOperation implements SampleConstraint {
   protected static final int BIT_VECTOR_SIZE = 32;
   protected static final DataType BIT_VECTOR_TYPE = DataType.BIT_VECTOR(BIT_VECTOR_SIZE);
@@ -56,9 +57,7 @@ public class BVUGTOperation implements SampleConstraint {
     final Formulas formulas = new Formulas();
     builder.setInnerRep(formulas);
 
-    formulas.add(new NodeOperation(
-      StandardOperation.BVUGT, x, new NodeValue(BIT_VECTOR_TYPE.valueOf("100", 10))));
-
+    formulas.add(Nodes.BVUGT(x, new NodeValue(BIT_VECTOR_TYPE.valueOf("100", 10))));
     return builder.build();
   }
 
--- src/test/java/ru/ispras/fortress/solver/constraint/ConstraintConjunctionTestCase.java
+++ src/test/java/ru/ispras/fortress/solver/constraint/ConstraintConjunctionTestCase.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014 ISP RAS (http://www.ispras.ru)
- * 
+ * Copyright 2014-2017 ISP RAS (http://www.ispras.ru)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -18,8 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ru.ispras.fortress.data.Variable;
-import ru.ispras.fortress.expression.NodeOperation;
-import ru.ispras.fortress.expression.StandardOperation;
+import ru.ispras.fortress.expression.Nodes;
 
 public class ConstraintConjunctionTestCase extends GenericSolverTestBase {
   public ConstraintConjunctionTestCase() {
@@ -38,19 +37,16 @@ public class ConstraintConjunctionTestCase extends GenericSolverTestBase {
       builder.setKind(constraint.getKind());
       builder.setName(conjunction.getName());
 
-      for (Variable var : conjunction.getVariables()) {
+      for (final Variable var : conjunction.getVariables()) {
         builder.addVariable(var.getName(), var.getData());
       }
 
       final Formulas formulas = new Formulas();
       builder.setInnerRep(formulas);
 
-      formulas.add(new NodeOperation(
-        StandardOperation.NOT,
-        new NodeOperation(StandardOperation.AND,
+      formulas.add(Nodes.NOT(Nodes.AND(
           ((Formulas) conjunction.getInnerRep()).exprs().iterator().next(),
-          ((Formulas) conjunction.getInnerRep()).exprs().iterator().next()))
-      );
+          ((Formulas) conjunction.getInnerRep()).exprs().iterator().next())));
 
       return builder.build();
     }
