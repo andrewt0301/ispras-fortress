50a8ab1527a3bcd760d33ab7486b9e79aca5a082
Andrei Tatarnikov <andrewt@ispras.ru>
2018-01-10 18:13:32 +0300

    Small update
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/ru/ispras/fortress/util/Result.java
+++ src/main/java/ru/ispras/fortress/util/Result.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2015 ISP RAS (http://www.ispras.ru)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -18,16 +18,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static ru.ispras.fortress.util.InvariantChecks.checkNotNull;
-
 public class Result<E extends Enum<E>, T> {
   private final E status;
   private final T result;
   private final List<String> errors;
 
   public Result(final E status, final T result, final List<String> errors) {
-    checkNotNull(status);
-    checkNotNull(errors);
+    InvariantChecks.checkNotNull(status);
+    InvariantChecks.checkNotNull(errors);
 
     this.status = status;
     this.result = result;
