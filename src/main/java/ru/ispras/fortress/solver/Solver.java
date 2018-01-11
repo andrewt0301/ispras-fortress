c38c7dcfec8b6e558373fc6a2de04d55aa9ca85b
Andrei Tatarnikov <andrewt@ispras.ru>
2018-01-11 11:35:13 +0300

    JavaDoc fix
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/ru/ispras/fortress/solver/Solver.java
+++ src/main/java/ru/ispras/fortress/solver/Solver.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2011-2015 ISP RAS (http://www.ispras.ru)
- * 
+ * Copyright 2011-2017 ISP RAS (http://www.ispras.ru)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -20,9 +20,9 @@ import ru.ispras.fortress.solver.function.Function;
 import ru.ispras.fortress.solver.function.FunctionTemplate;
 
 /**
- * The Solver interface provides a protocol for working with different kinds of constraint solvers
- * in a universal way.
- * 
+ * The {@link Solver} interface provides a protocol for working with different kinds of constraint
+ * solvers in a universal way.
+ *
  * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
  */
 public interface Solver {
@@ -64,10 +64,12 @@ public interface Solver {
   SolverResult solve(Constraint constraint);
 
   /**
-   * Register a custom operation that extends the functionality of the solver. The operation is
-   * implemented in terms of existing operations and represents a function.
-   * 
+   * Register a custom operation that extends the functionality of the solver.
+   * The operation is implemented in terms of existing operations and represents a function.
+   *
    * @param function Object describing the semantics and syntax of the function.
+   * @return {@code true} if no such operation has been previously registered or
+   *         {@code false} otherwise.
    */
   boolean addCustomOperation(Function function);
 
@@ -75,9 +77,11 @@ public interface Solver {
    * Register a custom operation that extends the functionality of the solver. The operation is
    * implemented in terms of existing operation and represents a family of functions derived from
    * the same template. Functions share the same logic, but may operate on different data types.
-   * 
+   *
    * @param template Function template that describes the semantics and syntax of a family of
    *        similar functions.
+   * @return {@code true} if no such operation has been previously registered or
+   *         {@code false} otherwise.
    */
   boolean addCustomOperation(FunctionTemplate template);
 
