1df2d62f027ac47deb95ecf5b2f0c4db67f8cc77
Sergey Smolov <smolov@ispras.ru>
2018-08-15 11:55:20 +0300

    [Expression] convert non-const params to 0 only if their number > 1
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- src/main/java/ru/ispras/fortress/expression/NodeOperation.java
+++ src/main/java/ru/ispras/fortress/expression/NodeOperation.java
@@ -262,7 +262,7 @@ public final class NodeOperation extends Node {
     /* If parameters are non-constant but equal, treat them as zeros. */
     final Node firstParam = operands.get(0);
 
-    if (Kind.VALUE != firstParam.getKind()) {
+    if (Kind.VALUE != firstParam.getKind() && paramCount > 1) {
 
       boolean equalParams = true;
 
